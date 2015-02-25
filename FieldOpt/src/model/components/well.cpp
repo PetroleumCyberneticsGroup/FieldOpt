181e176d1acb16dacdf52797671f4c37fc3748e2
Einar Baumann <einar.baumann@gmail.com>
2015-02-25 13:11:07 +0100

    Well class done

--- FieldOpt/src/model/components/well.cpp
+++ FieldOpt/src/model/components/well.cpp
@@ -22,13 +22,84 @@
 
 #include "well.h"
 
-Well::Well()
-{
+Well::Well() :
+    m_bhp_inj(WellControl::QWAT),
+    p_cost(0),
+    p_well_path(0)
+{}
 
+Well::Well(const Well &w) :
+    Component(w),
+    p_cost(0),
+    p_well_path(0)
+{
+    m_type = w.m_type;
+    m_name = w.m_name;
+    m_group = w.m_group;
+    m_bhp_limit = w.m_bhp_limit;
+    m_bhp_inj = w.m_bhp_inj;
+
+    for(int i = 0; i < w.m_connections.size(); i++)
+        m_connections.push_back(new WellConnection(*w.m_connections.at(i)));
+
+    for(int i = 0; i < w.m_var_connections.size(); ++i)
+        m_var_connections.push_back(new WellConnectionVariable(*w.m_var_connections.at(i)));
+
+    for(int i = 0; i < w.numberOfControls(); i++)
+        m_schedule.push_back(new WellControl(*w.m_schedule.at(i)));
+
+    if(w.p_cost != 0)
+        p_cost = new Cost(*w.p_cost);
+    if(w.p_install_time != 0)
+        p_install_time = shared_ptr<IntVariable>(new IntVariable(*w.p_install_time));
+    if(w.p_well_path != 0)
+        p_well_path = new WellPath(*w.p_well_path);
 }
 
 Well::~Well()
 {
+    for(int i = 0; i < m_connections.size(); i++)
+        delete m_connections.at(i);
 
+    for(int i = 0; i < m_var_connections.size(); ++i)
+        delete m_var_connections.at(i);
+
+    for(int i = 0; i < m_schedule.size(); i++)
+        delete m_schedule.at(i);
+
+    if(p_cost != 0)
+        delete p_cost;
+    if(p_well_path != 0)
+        delete p_well_path;
 }
 
+void Well::initialize()
+{
+    clearStreams();
+    for(int i = 0; i < m_schedule.size(); ++i)
+        addStream(new Stream(m_schedule.at(i)->endTime(), 0, 0, 0, 0));
+}
+
+bool Well::isInstalled(int i)
+{
+    if(p_install_time == 0)
+        return true;
+    else
+        return (i >= p_install_time->value());
+}
+
+void Well::addVariableConnection(WellConnectionVariable *vc)
+{
+    vc->iVariable()->setName("I-direction connection variable for well: " + name());
+    vc->jVariable()->setName("J-direction connection variable for well: " + name());
+    m_var_connections.push_back(vc);
+}
+
+WellConnection *Well::connection(int i) const
+{
+    if(i < m_connections.size())
+        return m_connections.at(i);
+    else if(i < (m_connections.size() + m_var_connections.size()))
+        return m_var_connections.at(i - m_connections.size())->wellConnection();
+    else return 0;
+}
--- FieldOpt/src/model/components/well.h
+++ FieldOpt/src/model/components/well.h
@@ -26,15 +26,22 @@
 #include <QVector>
 #include "tr1/memory"
 #include "model/components/component.h"
+#include "model/stream.h"
+#include "model/wellcontrol.h"
+#include "model/wellconnection.h"
+#include "model/wellconnectionvariable.h"
+#include "model/wellpath.h"
+#include "model/cost.h"
 #include "variables/intvariable.h"
 
 using std::tr1::shared_ptr;
 
-class WellControl;
 class WellConnection;
 class WellConnectionVariable;
 class WellPath;
+class Stream;
 class Cost;
+class IntVariable;
 
 /*!
  * \brief Base class for representation of wells.
@@ -60,8 +67,58 @@ private:
     shared_ptr<IntVariable> p_install_time;  //!< The installation time of the separator.
 
 public:
-    Well();
-    ~Well();
+    Well();               //!< Default constructor. Initialize the injector control type to QWAT, and cost and well path to 0.
+    Well(const Well &w);  //!< Copy constructor.
+    virtual ~Well();      //!< Default destructor.
+
+    void initialize();                                                        //!< Initialize all the streams by adding one Stream for each schedule entry.
+    bool isInstalled(int i);                                                  //!< Check whether the well is installed at schedule entry i.
+    bool hasInstallTime() const {return p_install_time != 0;}                 //!<
+    bool hasCost() const {return p_cost != 0;}                                //!<
+    bool hasVariableConnections() const {return m_var_connections.size()>0;}  //!<
+    bool hasWellPath() const {return p_well_path != 0;}                       //!<
+
+    virtual Well* clone() const = 0;          //!<
+    virtual void setAutomaticType() = 0;      //!<
+    virtual QString description() const = 0;  //!<
+
+    void addConnection(WellConnection *c) {m_connections.push_back(c);}  //!< Add a new connection (perforation) to the well.
+    void addVariableConnection(WellConnectionVariable *vc);              //!< Add a new WellConnectionVariable to the well.
+    void addControl(WellControl *c) {m_schedule.push_back(c);}           //!< Add a new control WellControl (schedule entry) to the well's schedule.
+    void setType(Well::well_type t) {m_type = t;}                        //!< Set the type of well (injector or producer)
+    void setName(const QString &n) {m_name = n;}                         //!< Set the Well name.
+    void setGroup(const QString &g) {m_group = g;}                       //!< Set the group that the well belongs to.
+    void setWellPath(WellPath *wp) {p_well_path = wp;}                   //!< Set the WellPath for this well. WellPath's are used by the MRST reservoir simulator.
+    /*!
+     * \brief Set the BHP (bottom hole pressure) limit for this Well.
+     *
+     * The BHP limit is the minimum allowed pressure for producers and the maximum allowed pressure for injectors.
+     * The limit is only used when the well is on rate control, and is not considered a constraint.
+     * \param p
+     */
+    void setBhpLimit(double p) {m_bhp_limit = p;}
+    void setBhpInj(WellControl::contol_type t) {m_bhp_inj = t;}  //!< Set the default injection phase when injection well is on BHP control (BHP, QOIL, QGAS or QWAT)
+    void setCost(Cost *c) {p_cost = c;}                                   //!< Set the cost of this well. \todo This is not currently implemented in the Cost class.
+    void setInstallTime(shared_ptr<IntVariable> t) {p_install_time = t;}  //!< Set the install time of this well. \todo This is not currently implemented in the Cost class.
+
+
+    Well::well_type type() const {return m_type;}                                                //!< Get the well type.
+    QString typeAsString() const { return (m_type == Well::PROD ? "P" : "I"); }                  //!< Returns "P" if the well is a producer; otherwise "I".
+    const QString& name() const {return m_name;}                                                 //!< Get the well name.
+    const QString& group() const {return m_group;}                                               //!< Get the group that the well belongs to.
+    double bhpLimit() const {return m_bhp_limit;}                                                //!< Get the BHP limit of the well.
+    WellControl::contol_type bhpInj() const {return m_bhp_inj;}                                  //!< Get the injected phase when on BHP control.
+    int numberOfConnections() const {return (m_connections.size() + m_var_connections.size());}  //!< Get the number of cells that the well is connected to (i.e. the number of perforations).
+    int numberOfVariableConnections() const {return m_var_connections.size();}                   //!< Get the the number of variable connections.
+    int numberOfConstantConnections() const {return m_connections.size();}                       //!< Get the number of constant connections.
+    WellConnection* connection(int i) const;                                                     //!< Get the cell number of connection i.
+    WellConnectionVariable* variableConnection(int i) const {return m_var_connections.at(i);}    //!< Get the variable connection at position i.
+    WellConnection* constantConnection(int i) const {return m_connections.at(i);}                //!< Get the constant connection at position i.
+    int numberOfControls() const {return m_schedule.size();}                                     //!< Get the number of well control set points (schedule entries) for this well.
+    WellControl* control(int i) const {return m_schedule.at(i);}                                 //!< Get WellControl i from the schedule.
+    Cost* cost() const {return p_cost;}                                                          //!< Get the cost of this well. \todo Cost of wells not yet implemented in the Cost class.
+    shared_ptr<IntVariable> installTime() const {return p_install_time;}                         //!< Get the install time for the well. \todo Cost of wells not yet implemented in the Cost class.
+    WellPath* wellPath() {return p_well_path;}                                                   //!< Get the WellPath associated with this Well.
 };
 
 #endif // WELL_H
