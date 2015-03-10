aa2f65bdd4ef43fa87dcd3a2874f0a7e060048ea
Einar Baumann <einar.baumann@gmail.com>
2015-03-10 14:39:14 +0100

    Adjustments in readme and boost download script

--- libs/get_boost.sh
+++ libs/get_boost.sh
@@ -2,15 +2,3 @@
 echo "Downloading boost ..."
 wget -c 'http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.bz2/download'
 
-echo "Uncompressing boost ..."
-tar xf download
-
-echo "Deleting downloaded tar ..."
-rm download
-
-echo "Configuring boost installer ..."
-cd boost_1_57_0
-./bootstrap.sh --with-libraries=mpi,serialization --prefix=./boost
-
-echo "Compiling and installing ..."
-./b2
