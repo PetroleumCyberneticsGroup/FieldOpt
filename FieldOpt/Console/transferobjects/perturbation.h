0be7af65857f984821f4184f930eead8d3245751
Einar Baumann <einar.baumann@gmail.com>
2015-04-17 10:53:21 +0200

    Add some doxygen comments.

--- FieldOpt/Console/transferobjects/perturbation.h
+++ FieldOpt/Console/transferobjects/perturbation.h
@@ -8,33 +8,45 @@
 #include "variables/intvariable.h"
 #include "variables/realvariable.h"
 
+/*!
+ * \brief The Perturbation class is a convenience class used for handling the variables of
+ * perturbed Case objects before and after transfering them using MPI.
+ */
 class Perturbation
 {
 private:
-    const int perturbation_id;
-    const int binaryLength;
-    const int integerLength;
-    const int realLength;
-    std::vector<double> binaryVariables;
-    std::vector<int> integerVariables;
-    std::vector<double> realVariables;
+    const int perturbation_id;              //!< A unique ID for the perturbation. After evaluation a Result object with the same ID should be created.
+    const int binaryLength;                 //!< The number of binary variables.
+    const int integerLength;                //!< The number of integer variables.
+    const int realLength;                   //!< The number of real variables.
+    std::vector<double> binaryVariables;    //!< List containing all the binary variables from a case.
+    std::vector<int> integerVariables;      //!< List containing all the integer variables from a case.
+    std::vector<double> realVariables;      //!< List containing all the real variables from a case.
     Case* c;
 
 public:
-    Perturbation(Case* c, int id);
+    Perturbation(Case* c, int id);  //!< Create a Pertrubation object from a Case and an id.
 
     Case* getCase(Model* m);  //!< Creates a Case object by combining information from the variables stored in this class and the information in a Model object.
 
-    int getPerturbation_id() const { return perturbation_id; }
+    int getPerturbation_id() const { return perturbation_id; }  //!< Get the id.
 
-    int getBinaryLength() const { return binaryLength; }
-    int getIntegerLength() const { return integerLength; }
-    int getRealLength() const { return realLength; }
+    int getBinaryLength() const { return binaryLength; }    //!< Get the number of binary variables.
+    int getIntegerLength() const { return integerLength; }  //!< Get the number of integer variables.
+    int getRealLength() const { return realLength; }        //!< Get the number of real variables.
 
+    /*!
+     * \brief Get the "send header".
+     *
+     * The send should be sent before the rest of the contents of a Perturbation object.
+     * It describes what is to be received: the id of the perturbation, as well as the number
+     * of variables of each type.
+     * \return Vector containing: perturbation_id, binaryLength, integerLength, realLength
+     */
     std::vector<int> getSendHeader() const;
-    std::vector<double> getBinaryVariables() const { return binaryVariables; }
-    std::vector<int> getIntegerVariables() const { return integerVariables; }
-    std::vector<double> getRealVariables() const { return realVariables; }
+    std::vector<double> getBinaryVariables() const { return binaryVariables; }  //!< Get a vector containing all binary variables.
+    std::vector<int> getIntegerVariables() const { return integerVariables; }   //!< Get a vector containing all integer variables.
+    std::vector<double> getRealVariables() const { return realVariables; }      //!< Get a vector containing all real variables.
 };
 
 #endif // PERTURBATION_H
--- FieldOpt/Console/transferobjects/result.h
+++ FieldOpt/Console/transferobjects/result.h
@@ -3,17 +3,21 @@
 
 #include "optimizers/case.h"
 
+/*!
+ * \brief The Result class is a convenience class used for handling the result of
+ * a Case evaluation before and after transfering them using MPI.
+ */
 class Result
 {
 private:
-    const int perturbation_id;
-    const double result;
+    const int perturbation_id;  //!< A unique id for the result. Should correspond to the id of a Perturbation object.
+    const double result;        //!< The value of the result.
 
 public:
-    Result(Case* c, int id);
+    Result(Case* c, int id);  //!< Create a Result object from an evaluated Case and an id.
 
-    int getPerturbation_id() const { return perturbation_id; }
-    double getResult() const { return result; }
+    int getPerturbation_id() const { return perturbation_id; }  //!< Get the id of this result.
+    double getResult() const { return result; }                 //!< Get the value of the result.
 };
 
 #endif // RESULT_H
