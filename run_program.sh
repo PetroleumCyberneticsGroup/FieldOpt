709baa93513e0eee5b3b6f23a92c9782984e12b7
Einar Baumann <einar.baumann@gmail.com>
2015-04-30 16:58:55 +0200

    Increase number of processes

--- run_program.sh
+++ run_program.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mpirun -n 4 -x LD_LIBRARY_PATH build-FieldOpt-Desktop-Debug/Console/Console /home/einar/Documents/git/master/FieldOpt/examples/MRST/compass/driver.dat /home/einar/Documents/MATLAB/mrst-2013a
+mpirun -n 5 -x LD_LIBRARY_PATH build-FieldOpt-Desktop-Debug/Console/Console /home/einar/Documents/git/master/FieldOpt/examples/MRST/compass/driver.dat /home/einar/Documents/MATLAB/mrst-2013a
 
