8a0d42c6a48d2adf49eac9c3937e848b7d906722
Einar Baumann <einar.baumann@gmail.com>
2015-05-05 15:10:41 +0000

    Jobscripts done.

---
+++ jobscript_batch_part3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -N FieldOpt_batch_part_3
+#PBS -l nodes=2:ppn=12:default
+#PBS -l walltime=12:00:00
+#PBS -l pmem=1800MB
+#PBS -A acc-ipt
+#PBS -q default
+
+cd ${PBS_O_WORKDIR}/build/Console
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../src
+
+module load intelcomp/13.0.1
+module load openmpi/1.6.5
+module load python/2.7.6-intel
+module load boost
+module load qt
+module load matlab
+
+for P in 12 18 24
+do
+	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_24/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
+done
+
+for P in 12 18 24
+do
+	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_36/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
+done
