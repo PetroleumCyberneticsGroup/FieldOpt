81498e5312a51fb6513d05fe1852effe0141f081
Einar Baumann <einar.baumann@gmail.com>
2015-05-05 15:00:49 +0000

    Script ready for deplotment

--- jobscript_bath_part1.sh
+++ jobscript_bath_part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N FieldOpt_batch_part_1
 #PBS -l nodes=2:ppn=12:default
-#PBS -l walltime=00:10:00
+#PBS -l walltime=05:00:00
 #PBS -l pmem=1800MB
 #PBS -A acc-ipt
 #PBS -q express
@@ -16,22 +16,22 @@ module load boost
 module load qt
 module load matlab
 
-for P in 1 2 4 6 8 12 18 24
+for P in 1 2 6 12 18 24
 do
 	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_1/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
 done
 
-# for P in 1 2 4 6 8 12 18 24
-# do
-# 	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_2/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
-# done
+for P in 1 2 6 12 18 24
+do
+	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_2/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
+done
 
-# for P in 1 2 4 6 8 12 18 24
-# do
-# 	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_4/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
-# done
+for P in 1 2 6 12 18 24
+do
+	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_4/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
+done
 
-# for P in 1 2 4 6 8 12 18 24
-# do
-# 	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_8/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
-# done
+for P in 1 2 6 12 18 24
+do
+	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_8/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
+done
