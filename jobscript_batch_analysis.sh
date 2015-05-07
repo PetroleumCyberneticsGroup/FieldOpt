6d30f04295300e366bfd4486df047ae70eb976b4
Einar Baumann <einar.baumann@gmail.com>
2015-05-07 12:49:38 +0000

    Add jobscript for batch analysis

---
+++ jobscript_batch_analysis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -N FieldOpt_baselines
+#PBS -l nodes=1:ppn=12:default
+#PBS -l walltime=01:00:00
+#PBS -l pmem=1800MB
+#PBS -A acc-ipt
+#PBS -q express
+
+cd ${PBS_O_WORKDIR}/build/Console
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../src
+
+module load intelcomp/13.0.1
+module load openmpi/1.6.5
+module load python/2.7.6-intel
+module load boost
+module load qt
+module load matlab
+
+for P in 2 3 4 5 6 7 8 9 10 11 12
+do
+	mpirun -npernode $P -x LD_LIBRARY_PATH Console ${PBS_O_WORKDIR}/examples/MRST/bhp_4/driver.dat ${PBS_O_WORKDIR}/MRST/mrst-2013a
+done
