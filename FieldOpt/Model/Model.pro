f4741a811beed031a7a16353bd197517753eb2e2
Einar Baumann <einar.baumann@gmail.com>
2015-09-24 16:49:49 +0200

    Start creating CompletionCollection class

--- FieldOpt/Model/Model.pro
+++ FieldOpt/Model/Model.pro
@@ -33,7 +33,9 @@ HEADERS += \
     wells/trajectory/trajectory.h \
     wells/trajectory/wellspline.h \
     wells/trajectory/wellblocks.h \
-    wells/wellgroup.h
+    wells/wellgroup.h \
+    wells/completions/completioncollection.h \
+    wells/completions/completion_exceptions.h
 
 SOURCES += \
     model.cpp \
@@ -55,4 +57,5 @@ SOURCES += \
     wells/trajectory/trajectory.cpp \
     wells/trajectory/wellspline.cpp \
     wells/trajectory/wellblocks.cpp \
-    wells/wellgroup.cpp
+    wells/wellgroup.cpp \
+    wells/completions/completioncollection.cpp
---
+++ FieldOpt/Model/wells/completions/completioncollection.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *
+ * completioncollection.h
+ *
+ * Created: 24.09.2015 2015 by einar
+ *
+ * This file is part of the FieldOpt project.
+ *
+ * Copyright (C) 2015-2015 Einar J.M. Baumann <einar.baumann@ntnu.no>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *****************************************************************************/
+
+#ifndef COMPLETIONCOLLECTION_H
+#define COMPLETIONCOLLECTION_H
+
+#include <QList>
+
+#include "perforation.h"
+#include "inflowcontroldevice.h"
+
+namespace Model {
+namespace Wells {
+namespace Completions {
+
+class CompletionCollection
+{
+public:
+    CompletionCollection();
+
+    void AddPerforation(Perforation *perf);
+    void AddInflowControlDevice(InflowControlDevice *icd);
+
+private:
+    QList<Perforation *> *perforations_;
+    QList<InflowControlDevice *> *inflow_control_devices_;
+
+    bool NewCompletionIsValid(Perforation *perf);
+    bool NewCompletionIsValid(InflowControlDevice *icd);
+};
+
+}
+}
+}
+
+#endif // COMPLETIONCOLLECTION_H
