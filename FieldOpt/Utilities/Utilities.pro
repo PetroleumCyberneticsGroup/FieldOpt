1241eae57bacf0f06aa3c1450f6030545af40f71
Einar Baumann <einar.baumann@gmail.com>
2015-09-28 11:33:52 +0200

    Add stubs for settings classes

--- FieldOpt/Utilities/Utilities.pro
+++ FieldOpt/Utilities/Utilities.pro
@@ -6,7 +6,15 @@ TEMPLATE = lib
 TARGET = utilities
 
 HEADERS += \
-    driver/reader.h
+    driver/reader.h \
+    settings/settings.h \
+    settings/optimizer.h \
+    settings/simulator.h \
+    settings/model.h
 
 SOURCES += \
-    driver/reader.cpp
+    driver/reader.cpp \
+    settings/settings.cpp \
+    settings/optimizer.cpp \
+    settings/simulator.cpp \
+    settings/model.cpp
---
+++ FieldOpt/Utilities/settings/optimizer.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *
+ * optimizer.h
+ *
+ * Created: 28.09.2015 2015 by einar
+ *
+ * This file is part of the FieldOpt project.
+ *
+ * Copyright (C) 2015-2015 Einar J.M. Baumann <einar.baumann@ntnu.no>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *****************************************************************************/
+
+#ifndef OPTIMIZER_H
+#define OPTIMIZER_H
+
+#include "settings.h"
+
+namespace Utilities {
+namespace Settings {
+
+class Optimizer : public Settings
+{
+public:
+    Optimizer();
+};
+
+}
+}
+
+#endif // OPTIMIZER_H
