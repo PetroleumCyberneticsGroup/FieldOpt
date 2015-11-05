fbefdc76d2de8e9cf6ead3e981517f7931c030b1
Einar Baumann <einar.baumann@gmail.com>
2015-11-05 11:16:25 +0100

    Add some documentation

--- FieldOpt/Model/reservoir/reservoir.h
+++ FieldOpt/Model/reservoir/reservoir.h
@@ -32,12 +32,16 @@
 namespace Model {
 namespace Reservoir {
 
+/*!
+ * \brief The Reservoir class represents the entirety of the reservoir. It acts as a
+ * wrapper for the grid.
+ */
 class Reservoir
 {
 public:
     Reservoir(::Utilities::Settings::Model::Reservoir reservoir_settings);
 
-    Grid::Grid* grid() const { return grid_; }
+    Grid::Grid* grid() const { return grid_; } //!< Get the grid associated with the reservoir.
 
 private:
     Grid::Grid* grid_;
