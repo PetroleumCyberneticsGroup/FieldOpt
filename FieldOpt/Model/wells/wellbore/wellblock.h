f5a07981c6591f24311af8facc343e02dc97d863
Einar Baumann <einar.baumann@gmail.com>
2016-06-08 14:08:42 +0200

    Wrote destructor

--- FieldOpt/Model/wells/wellbore/wellblock.h
+++ FieldOpt/Model/wells/wellbore/wellblock.h
@@ -1,28 +1,3 @@
-/******************************************************************************
- *
- *
- *
- * Created: 29.10.2015 2015 by einar
- *
- * This file is part of the FieldOpt project.
- *
- * Copyright (C) 2015-2015 Einar J.M. Baumann <einar.baumann@ntnu.no>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *****************************************************************************/
-
 #ifndef WELLBLOCK_H
 #define WELLBLOCK_H
 
@@ -44,7 +19,9 @@ class WellBlock
 public:
     WellBlock(int i, int j, int k);
 
-    /*!
+    virtual ~WellBlock();
+
+/*!
      * \brief The DirectionOfPenetration enum indicates the wells direction of penetration through
      * this block. The W value should be used to indicate that the direction could not be calculated.
      */
