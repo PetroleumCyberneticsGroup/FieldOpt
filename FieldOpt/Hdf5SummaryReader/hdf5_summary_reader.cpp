a12fb94a8d12b57c4a787afba78d192b4c9e9cbb
Einar Baumann <einar.baumann@gmail.com>
2016-07-01 10:19:22 +0200

    Now reading well states

--- FieldOpt/Hdf5SummaryReader/hdf5_summary_reader.cpp
+++ FieldOpt/Hdf5SummaryReader/hdf5_summary_reader.cpp
@@ -22,11 +22,11 @@ std::vector<double> Hdf5SummaryReader::readTimeVector(std::string file_path) {
         throw std::runtime_error("Unexpected datatype for TIME vector.");
 
     DataSpace dataspace = dataset.getSpace();
-    hsize_t dims_out[2];
-    dataspace.getSimpleExtentDims(dims_out, NULL);
+    hsize_t dims[2];
+    dataspace.getSimpleExtentDims(dims, NULL);
 
     std::vector<double> vector;
-    vector.resize(dims_out[0]);
+    vector.resize(dims[0]);
     dataset.read(vector.data(), PredType::NATIVE_DOUBLE);
     return vector;
 }
@@ -37,24 +37,60 @@ void Hdf5SummaryReader::readWellStates(std::string file_path) {
     Group group = Group(file.openGroup(GROUP_NAME_FLOW_TRANSPORT));
     DataSet dataset = DataSet(group.openDataSet(DATASET_NAME_WELL_STATES));
 
+    DataSpace dataspace = dataset.getSpace();
+    int rank = dataspace.getSimpleExtentNdims();
+    std::cout << "Rank: " << rank << std::endl;
+    hsize_t dims[2];
+    int ndims = dataspace.getSimpleExtentDims(dims, NULL);
+    std::cout << "Dims: " << ndims << " -- " << dims[0] << "x" << dims[1] << std::endl;
+
     typedef struct wstype_t {
-        std::vector<double> pressure;
-        hvl_t pressure_handle;
-        std::vector<double> temperature;
-        hvl_t temperature_handle;
-        std::vector<double> phaserate;
-        hvl_t phaserate_handle;
-        std::vector<double> phaseratesc;
-        hvl_t phaseratesc_handle;
-        std::vector<double> avgdensity;
-        hvl_t avgdensity_handle;
-        std::vector<int> intdata;
-        hvl_t intdata_handle;
-        std::vector<double> doubledata;
-        hvl_t doubledata_handle;
+        std::vector<double> vPressures;
+        hvl_t vPressures_handle;
+        std::vector<double> vTemperatures;
+        hvl_t vTemperatures_handle;
+        std::vector<double> vPhaseRates;
+        hvl_t vPhaseRates_handle;
+        std::vector<double> vPhaseRatesAtSC;
+        hvl_t vPhaseRatesAtSC_handle;
+        std::vector<double> vAverageDensity;
+        hvl_t vAverageDensity_handle;
+        std::vector<int> vIntData;
+        hvl_t vIntData_handle;
+        std::vector<double> vDoubleData;
+        hvl_t vDoubleData_handle;
     } wstype_t;
     CompType ctype(sizeof(wstype_t));
     auto double_type = PredType::NATIVE_DOUBLE;
     auto int_type = PredType::NATIVE_INT;
+    auto vdouble_type = VarLenType(&double_type);
+    auto vint_type = VarLenType(&int_type);
+    ctype.insertMember("vPressures", HOFFSET(wstype_t, vPressures_handle), vdouble_type);
+    ctype.insertMember("vTemperatures", HOFFSET(wstype_t, vTemperatures_handle), vdouble_type);
+    ctype.insertMember("vPhaseRates", HOFFSET(wstype_t, vPhaseRates_handle), vdouble_type);
+    ctype.insertMember("vPhaseRatesAtSC", HOFFSET(wstype_t, vPhaseRatesAtSC_handle), vdouble_type);
+    ctype.insertMember("vAverageDensity", HOFFSET(wstype_t, vAverageDensity_handle), vdouble_type);
+    ctype.insertMember("vIntData", HOFFSET(wstype_t, vIntData_handle), vint_type);
+    ctype.insertMember("vDoubleData", HOFFSET(wstype_t, vDoubleData_handle), vdouble_type);
+
+    std::vector<wstype_t> data_vector;
+    data_vector.resize(dims[0] * dims[1]);
+    dataset.read(data_vector.data(), ctype);
+    for (wstype_t &m : data_vector) {
+        m.vPressures.assign(static_cast<double*>(m.vPressures_handle.p),
+                            static_cast<double*>(m.vPressures_handle.p) + m.vPressures_handle.len);
+        m.vTemperatures.assign(static_cast<double*>(m.vTemperatures_handle.p),
+                               static_cast<double*>(m.vTemperatures_handle.p) + m.vTemperatures_handle.len);
+        m.vPhaseRates.assign(static_cast<double*>(m.vPhaseRates_handle.p),
+                             static_cast<double*>(m.vPhaseRates_handle.p) + m.vPhaseRates_handle.len);
+        m.vPhaseRatesAtSC.assign(static_cast<double*>(m.vPhaseRatesAtSC_handle.p),
+                                 static_cast<double*>(m.vPhaseRatesAtSC_handle.p) + m.vPhaseRatesAtSC_handle.len);
+        m.vAverageDensity.assign(static_cast<double*>(m.vAverageDensity_handle.p),
+                                 static_cast<double*>(m.vAverageDensity_handle.p) + m.vAverageDensity_handle.len);
+        m.vIntData.assign(static_cast<int*>(m.vIntData_handle.p),
+                          static_cast<int*>(m.vIntData_handle.p) + m.vIntData_handle.len);
+        m.vDoubleData.assign(static_cast<double*>(m.vDoubleData_handle.p),
+                             static_cast<double*>(m.vDoubleData_handle.p) + m.vDoubleData_handle.len);
+    }
 }
 
