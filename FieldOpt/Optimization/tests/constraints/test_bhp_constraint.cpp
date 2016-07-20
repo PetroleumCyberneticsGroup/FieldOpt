32ec5fd2a0a4c8549e8f53e83bb2de56b51ccb68
Bellout <chakibbb@gmail.com>
2016-07-20 02:03:23 +0200

    Added debug msg. Changed to more realistic simulation values according to new driver.json parameters.

--- FieldOpt/Optimization/tests/constraints/test_bhp_constraint.cpp
+++ FieldOpt/Optimization/tests/constraints/test_bhp_constraint.cpp
@@ -8,6 +8,11 @@ namespace {
     class BhpConstraintTest : public ::testing::Test, public TestResources::TestResourceOptimizer {
     protected:
         BhpConstraintTest() {
+            // USE FIRST DEFINED CONSTRAINT FOR BHP TESTING
+            if (settings_full_->verbosity()>2){
+                std::cout << "... using first constraint defined in driver.json (BHP); well: " << settings_optimizer_->constraints()[0].well.toStdString() << std::endl;
+            }
+
             box_constraint_ = new ::Optimization::Constraints::BhpConstraint(
                 settings_optimizer_->constraints()[0], model_->variables());
         }
@@ -32,21 +37,21 @@ namespace {
             bhp_ids.append(var->id());
         }
 
-        // set all bhps to 5000 (which exceeds the max value defined, 3000)
+        // set all bhps to 500 (which exceeds the max value defined, 300)
         for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 5000);
+            base_case_->set_real_variable_value(key, 500);
         }
         EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
 
-        // set all bhps to 100 (which is less than the min value defined, 1000)
+        // set all bhps to 50 (which is less than the min value defined, 100)
         for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 100);
+            base_case_->set_real_variable_value(key, 50);
         }
         EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
 
-        // set all bhps to 1125 (which is within the defined bounds)
+        // set all bhps to 125 (which is within the defined bounds)
         for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 1125);
+            base_case_->set_real_variable_value(key, 125);
         }
         EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
     }
@@ -58,18 +63,18 @@ namespace {
             bhp_ids.append(var->id());
         }
 
-        // set all bhps to 5000 (which exceeds the max value defined, 3000)
+        // set all bhps to 500 (which exceeds the max value defined, 300)
         for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 5000);
+            base_case_->set_real_variable_value(key, 500);
         }
         EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
         // Snap the values back and recheck
         box_constraint_->SnapCaseToConstraints(base_case_);
         EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
 
-        // set all bhps to 100 (which is less than the min value defined, 1000)
+        // set all bhps to 50 (which is less than the min value defined, 100)
         for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 100);
+            base_case_->set_real_variable_value(key, 50);
         }
         EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
         // Snap the values back and recheck
