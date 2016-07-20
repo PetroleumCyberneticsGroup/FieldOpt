4b8f65c0c6d45b68cc3688d66187868fc8be318b
Bellout <chakibbb@gmail.com>
2016-07-20 02:01:30 +0200

    Added debug messages. Added simple wellsplineconstraint test.

--- FieldOpt/Optimization/tests/constraints/test_constraint_handler.cpp
+++ FieldOpt/Optimization/tests/constraints/test_constraint_handler.cpp
@@ -8,6 +8,8 @@ namespace {
 class ConstraintHandlerTest : public ::testing::Test, public TestResources::TestResourceOptimizer, public TestResources::TestResourceGrids {
 protected:
     ConstraintHandlerTest() {
+		// ESTABLISH MODEL OBJECT FOR TESTING
+        if (settings_full_->verbosity()>2) std::cout << "... setting up constraint handler using test settings (test_constraint_handler.h)" << std::endl;
         constraint_handler_ = new ::Optimization::Constraints::ConstraintHandler(settings_optimizer_->constraints(),
                                                                                  model_->variables(), grid_5spot_);
     }
@@ -18,11 +20,18 @@ protected:
 };
 
 TEST_F(ConstraintHandlerTest, Constructor) {
-    EXPECT_EQ(5, constraint_handler_->constraints().size());
+    if (settings_full_->verbosity()>2) std::cout << "Number of constraint types defined in driver.json: " << constraint_handler_->constraints().size() << std::endl;
+    EXPECT_EQ(6, constraint_handler_->constraints().size());
 }
 
-TEST_F(ConstraintHandlerTest, BaseCaseSatasfaction) {
+TEST_F(ConstraintHandlerTest, BaseCaseSatisfyBHPConstraint) {
+    if (settings_full_->verbosity()>2) std::cout << "Check if base case satisfies first constraint type (BHP)" << std::endl;
     EXPECT_TRUE(constraint_handler_->constraints().first()->CaseSatisfiesConstraint(base_case_));
 }
 
+TEST_F(ConstraintHandlerTest, BaseCaseSatisfyWellSplineLengthConstraint) {
+    if (settings_full_->verbosity()>2) std::cout << "Check if base case breaks second constraint type (WellSplineLength)" << std::endl;
+    EXPECT_FALSE(constraint_handler_->constraints()[1]->CaseSatisfiesConstraint(base_case_));
+}
+    
 }
--- FieldOpt/Utilities/tests/test_resource_settings.h
+++ FieldOpt/Utilities/tests/test_resource_settings.h
@@ -3,25 +3,31 @@
 
 #include "Utilities/settings/settings.h"
 #include "Utilities/tests/test_resource_example_file_paths.h"
+#include <iostream>
 
 namespace TestResources {
 
     class TestResourceSettings {
     protected:
         TestResourceSettings() {
-                settings_full_ = new Utilities::Settings::Settings(ExampleFilePaths::driver_example_, ExampleFilePaths::directory_output_);
-                settings_optimizer_ = settings_full_->optimizer();
-                settings_simulator_ = settings_full_->simulator();
-                settings_model_ = settings_full_->model();
 
-                constraint_settings_reservoir_boundary_.type = Utilities::Settings::Optimizer::ConstraintType::ReservoirBoundary;
-                constraint_settings_reservoir_boundary_.box_imin = 1;
-                constraint_settings_reservoir_boundary_.box_imax = 1;
-                constraint_settings_reservoir_boundary_.box_jmin = 0;
-                constraint_settings_reservoir_boundary_.box_jmax = 0;
-                constraint_settings_reservoir_boundary_.box_kmin = 0;
-                constraint_settings_reservoir_boundary_.box_kmax = 0;
-                constraint_settings_reservoir_boundary_.well = "PRODUCER";
+            settings_full_ = new Utilities::Settings::Settings(ExampleFilePaths::driver_example_, ExampleFilePaths::directory_output_);
+            settings_optimizer_ = settings_full_->optimizer();
+            settings_simulator_ = settings_full_->simulator();
+            settings_model_ = settings_full_->model();
+
+            // ESTABLISH OVERALL SETTINGS FOR TESTING
+            if (settings_full_->verbosity()>2) std::cout << "... establishing overall settings for testing (test_resource_settings.h)" << std::endl;
+
+            // ESTABLISH RESERVOIR BOUNDARY SETTINGS FOR TESTING
+            constraint_settings_reservoir_boundary_.type = Utilities::Settings::Optimizer::ConstraintType::ReservoirBoundary;
+            constraint_settings_reservoir_boundary_.box_imin = 0;
+            constraint_settings_reservoir_boundary_.box_imax = 59;
+            constraint_settings_reservoir_boundary_.box_jmin = 0;
+            constraint_settings_reservoir_boundary_.box_jmax = 59;
+            constraint_settings_reservoir_boundary_.box_kmin = 0;
+            constraint_settings_reservoir_boundary_.box_kmax = 0;
+            constraint_settings_reservoir_boundary_.well = "TESTW";
         }
 
         Utilities::Settings::Settings *settings_full_;
