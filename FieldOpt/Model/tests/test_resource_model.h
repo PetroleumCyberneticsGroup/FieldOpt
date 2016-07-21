b2210607fa7e5b2f7eb92146d3afbe42b77f37fd
Bellout <chakibbb@gmail.com>
2016-07-21 03:13:05 +0200

    Added verbose debug msg.

--- FieldOpt/Model/tests/test_resource_model.h
+++ FieldOpt/Model/tests/test_resource_model.h
@@ -7,11 +7,14 @@
 
 #include "Model/model.h"
 #include "Utilities/tests/test_resource_settings.h"
+#include <iostream>
 
 namespace TestResources {
     class TestResourceModel : public TestResourceSettings {
     protected:
         TestResourceModel() {
+            // ESTABLISH MODEL OBJECT FOR TESTING
+            if (settings_full_->verbosity()>2) std::cout << "... establishing model object for testing (test_resource_model.h)" << std::endl;
             settings_model_->set_reservoir_grid_path(ExampleFilePaths::grid_5spot_);
             model_ = new Model::Model(*settings_model_);
         }
