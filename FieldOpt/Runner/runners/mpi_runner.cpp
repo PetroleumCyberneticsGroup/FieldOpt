5e81cdbdf251292e74fb3eafb81252881a9a71c5
Einar Baumann <einar.baumann@gmail.com>
2016-07-25 14:34:48 +0200

    Implemented send/rect methods

--- FieldOpt/Runner/runners/mpi_runner.cpp
+++ FieldOpt/Runner/runners/mpi_runner.cpp
@@ -1,13 +1,66 @@
-//
-// Created by einar on 7/20/16.
-//
-
 #include "mpi_runner.h"
+#include "Optimization/case_transfer_object.h"
+#include "Model/model_synchronization_object.h"
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/mpi/status.hpp>
+#include <iostream>
+
+BOOST_IS_MPI_DATATYPE(boost::uuids::uuid)
 
 namespace Runner {
 
     MPIRunner::MPIRunner(RuntimeSettings *rts) : AbstractRunner(rts) {
         rank_ = world_.rank();
+    }
+
+    void MPIRunner::SendCase(Optimization::Case *c, int dest, MPIRunner::MsgTag tag) {
+        auto cto = Optimization::CaseTransferObject(c);
+        std::ostringstream oss;
+        boost::archive::text_oarchive oa(oss);
+        oa << cto;
+        std::string s= oss.str();
+        world_.send(dest, tag, s);
+    }
+
+    Optimization::Case *MPIRunner::RecvCase(int &source, MPIRunner::MsgTag tag) {
+        Optimization::CaseTransferObject cto;
+        std::string s;
+        if (tag == CASE_EVAL) {
+            mpi::status status = world_.recv(mpi::any_source, tag, s);
+            source = status.source();
+        }
+        else {
+            mpi::status status = world_.recv(source, tag, s);
+        }
+        std::istringstream iss(s);
+        boost::archive::text_iarchive ia(iss);
+        ia >> cto;
+        auto c = cto.CreateCase();
+        return c;
+    }
+
+    void MPIRunner::BroadcastModel(Model::Model *model) {
+        if (rank() != 0) throw std::runtime_error("BroadcastModel should only be called on the root process.");
+        auto mso = Model::ModelSynchronizationObject(model);
+        std::ostringstream oss;
+        boost::archive::text_oarchive oa(oss);
+        oa << mso;
+        std::string s = oss.str();
+        for (int r = 1; r < world_.size(); ++r) {
+            world_.send(r, MODEL_SYNC, s);
+        }
+    }
+
+    void MPIRunner::RecvModelSynchronizationObject(Model::Model *model) {
+        if (rank() == 0) std::runtime_error("BroadcastModel should not be called on the root process.");
+        Model::ModelSynchronizationObject mso;
+        std::string s;
+        world_.recv(0, MODEL_SYNC, s);
+        std::istringstream iss(s);
+        boost::archive::text_iarchive ia(iss);
+        ia >> mso;
+        mso.UpdateVariablePropertyIds(model);
     }
 
 }
--- FieldOpt/Runner/runners/mpi_runner.h
+++ FieldOpt/Runner/runners/mpi_runner.h
@@ -27,11 +27,55 @@ namespace Runner {
         mpi::communicator &world() { return world_; }
         int rank() const { return rank_; }
 
+        /*!
+         * @brief Tags used when sending and receiving.
+         *
+         * CASE_UNEVAL: To be used when sending unevaluated cases.
+         * CASE_EVAL: To be used when sending evaluated cases.
+         * MODEL_SYNC: To be used when sending model synchronization objects.
+         */
+        enum MsgTag : int { CASE_UNEVAL=1, CASE_EVAL=2, MODEL_SYNC=3 };
+
     protected:
         MPIRunner(RuntimeSettings *rts);
         mpi::environment env_;
         mpi::communicator world_;
         int rank_;
+
+        /*!
+         * @brief Send a case to the specified destination.
+         * @param c Case to be sent.
+         * @param dest The rank of the process the case should be sent to.
+         * @param tag Tag to be used when sending.
+         */
+        void SendCase(Optimization::Case *c, int dest, MsgTag tag);
+
+
+        /*!
+         * @brief Receive a message with the specified tag containing a Case from the specified soruce.
+         * If the CASE_EVAL tag is passed, i.e. if we want to receive an evaluated case, a case will be received
+         * from any source, and the source argument will be set to the rank of the process the case was received from.
+         * @param source
+         * @param tag
+         * @return
+         */
+        Optimization::Case * RecvCase(int & source, MsgTag tag);
+
+        /*!
+         * @brief Create a ModelSynchronizationObject and send it to all other processes.
+         *
+         * This should be called by the process with rank 0, in order to make the variable UUIDs match across
+         * all processes.
+         * @param model The model to be broadcast.
+         */
+        void BroadcastModel(Model::Model *model);
+
+        /*!
+         * @brief Receive the ModelSynchronizationObject broadcast by the root process. This is applied to the
+         * model object.
+         * @param model The model to apply the synchronization object on.
+         */
+        void RecvModelSynchronizationObject(Model::Model *model);
     };
 
 }
