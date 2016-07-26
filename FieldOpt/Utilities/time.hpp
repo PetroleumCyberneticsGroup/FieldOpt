86e12b6e26c66d51fd5cbf98c1aad5f855426f8d
Einar Baumann <einar.baumann@gmail.com>
2016-07-26 14:47:15 +0200

    Make functions inline

--- FieldOpt/Utilities/time.hpp
+++ FieldOpt/Utilities/time.hpp
@@ -1,13 +1,13 @@
 /// This file contains helper methods for working with time.
-#ifndef TIME_FUNCTIONS
-#define TIME_FUNCTION
+#ifndef TIME_FUNCTIONS_H
+#define TIME_FUNCTIONS_H
 
 /*!
  * @brief Get the number of seconds from t1 to t2.
  * @param t1 Fist time. Should be before t2.
  * @param t2 Seconds time. Should be after t1.
  */
-int time_span_seconds(QDateTime t1, QDateTime t2) {
+inline int time_span_seconds(QDateTime t1, QDateTime t2) {
     return t2.toMSecsSinceEpoch()/1000 - t1.toMSecsSinceEpoch()/1000;
 }
 
@@ -15,8 +15,8 @@ int time_span_seconds(QDateTime t1, QDateTime t2) {
  * @brief Get the number of seconds that have passed from t1 until now.
  * @param t The time to get the number of seconds from.
  */
-int time_since_seconds(QDateTime t) {
+inline int time_since_seconds(QDateTime t) {
     return time_span_seconds(t, QDateTime::currentDateTime());
 }
 
-#endif // TIME_FUNCTIONS
+#endif // TIME_FUNCTIONS_H
