16b848b000579c49875eb34920f69dada7294ae5
Einar Baumann <einar.baumann@gmail.com>
2016-07-26 14:15:40 +0200

    Implemented Overseer

--- FieldOpt/Runner/runners/mpi_runner.cpp
+++ FieldOpt/Runner/runners/mpi_runner.cpp
@@ -9,58 +9,59 @@
 BOOST_IS_MPI_DATATYPE(boost::uuids::uuid)
 
 namespace Runner {
+    namespace MPI {
 
-    MPIRunner::MPIRunner(RuntimeSettings *rts) : AbstractRunner(rts) {
-        rank_ = world_.rank();
-    }
-
-    void MPIRunner::SendCase(Optimization::Case *c, int dest, MPIRunner::MsgTag tag) {
-        auto cto = Optimization::CaseTransferObject(c);
-        std::ostringstream oss;
-        boost::archive::text_oarchive oa(oss);
-        oa << cto;
-        std::string s= oss.str();
-        world_.send(dest, tag, s);
-    }
-
-    Optimization::Case *MPIRunner::RecvCase(int &source, MPIRunner::MsgTag tag) {
-        Optimization::CaseTransferObject cto;
-        std::string s;
-        if (tag == CASE_EVAL) {
-            mpi::status status = world_.recv(mpi::any_source, tag, s);
-            source = status.source();
+        MPIRunner::MPIRunner(RuntimeSettings *rts) : AbstractRunner(rts) {
+            rank_ = world_.rank();
         }
-        else {
-            mpi::status status = world_.recv(source, tag, s);
+
+        void MPIRunner::SendCase(Optimization::Case *c, int dest, MPIRunner::MsgTag tag) {
+            auto cto = Optimization::CaseTransferObject(c);
+            std::ostringstream oss;
+            boost::archive::text_oarchive oa(oss);
+            oa << cto;
+            std::string s = oss.str();
+            world_.send(dest, tag, s);
         }
-        std::istringstream iss(s);
-        boost::archive::text_iarchive ia(iss);
-        ia >> cto;
-        auto c = cto.CreateCase();
-        return c;
-    }
 
-    void MPIRunner::BroadcastModel() {
-        if (rank() != 0) throw std::runtime_error("BroadcastModel should only be called on the root process.");
-        auto mso = Model::ModelSynchronizationObject(model_);
-        std::ostringstream oss;
-        boost::archive::text_oarchive oa(oss);
-        oa << mso;
-        std::string s = oss.str();
-        for (int r = 1; r < world_.size(); ++r) {
-            world_.send(r, MODEL_SYNC, s);
+        Optimization::Case *MPIRunner::RecvCase(int &source, MPIRunner::MsgTag tag) {
+            Optimization::CaseTransferObject cto;
+            std::string s;
+            if (tag == CASE_EVAL) {
+                mpi::status status = world_.recv(mpi::any_source, tag, s);
+                source = status.source();
+            }
+            else {
+                mpi::status status = world_.recv(source, tag, s);
+            }
+            std::istringstream iss(s);
+            boost::archive::text_iarchive ia(iss);
+            ia >> cto;
+            auto c = cto.CreateCase();
+            return c;
+        }
+
+        void MPIRunner::BroadcastModel() {
+            if (rank() != 0) throw std::runtime_error("BroadcastModel should only be called on the root process.");
+            auto mso = Model::ModelSynchronizationObject(model_);
+            std::ostringstream oss;
+            boost::archive::text_oarchive oa(oss);
+            oa << mso;
+            std::string s = oss.str();
+            for (int r = 1; r < world_.size(); ++r) {
+                world_.send(r, MODEL_SYNC, s);
+            }
+        }
+
+        void MPIRunner::RecvModelSynchronizationObject() {
+            if (rank() == 0) std::runtime_error("BroadcastModel should not be called on the root process.");
+            Model::ModelSynchronizationObject mso;
+            std::string s;
+            world_.recv(0, MODEL_SYNC, s);
+            std::istringstream iss(s);
+            boost::archive::text_iarchive ia(iss);
+            ia >> mso;
+            mso.UpdateVariablePropertyIds(model_);
         }
     }
-
-    void MPIRunner::RecvModelSynchronizationObject() {
-        if (rank() == 0) std::runtime_error("BroadcastModel should not be called on the root process.");
-        Model::ModelSynchronizationObject mso;
-        std::string s;
-        world_.recv(0, MODEL_SYNC, s);
-        std::istringstream iss(s);
-        boost::archive::text_iarchive ia(iss);
-        ia >> mso;
-        mso.UpdateVariablePropertyIds(model_);
-    }
-
 }
--- FieldOpt/Runner/runners/overseer.h
+++ FieldOpt/Runner/runners/overseer.h
@@ -2,6 +2,7 @@
 #define FIELDOPT_OVERSEER_H
 
 #include "mpi_runner.h"
+#include "Utilities/time.hpp"
 
 namespace Runner {
     namespace MPI {
@@ -14,8 +15,62 @@ namespace Runner {
 
             Overseer(MPIRunner *runner);
 
+            /*!
+             * @brief Assign a Case to a Worker. If no workers are free, the scheduler will wait untill one is.
+             * @param c The case to be assigned to a Worker for evaluation.
+             */
+            void AssignCase(Optimization::Case *c);
+
+            /*!
+             * @brief Wait to receive an evaluated case.
+             * @return An evaluated case object.
+             */
+            Optimization::Case *RecvEvaluatedCase();
+
+            /*!
+             * @brief Get the number of workers that are currently not performing any work.
+             */
+            int NumberOfFreeWorkers();
+
+            /*!
+             * @brief The WorkerStatus struct holds information about the current status of all workers in the network.
+             */
+            struct WorkerStatus {
+                WorkerStatus(int r) { rank = r;}
+                int rank; //!< The rank of the process the worker is running on.
+                bool working = false; //!< Indicates if the worker is currently performing simulations.
+                QDateTime working_since; //!< The last time a job was sent to the worker.
+                int working_seconds() { //!< Number of seconds since last work was sent to the process.
+                    return time_since_seconds(working_since);
+                }
+                /*!
+                 * @brief Start the worker. Should be called whenever work is sent to the worker. This marks is as
+                 * working.
+                 */
+                void start() {
+                    working = true;
+                    working_since = QDateTime::currentDateTime();
+                }
+                /*!
+                 * @brief Stop the worker. This should be called whenever results are received from the worker. This
+                 * marks the worker as not working.
+                 */
+                void stop() {
+                    working = false;
+                }
+            };
+
+            /*!
+                 * @brief Get the status for the longest running worker.
+                 * @return A copy of the status object for the longest running worker.
+                 */
+            WorkerStatus GetLongestRunningWorker();
+
         private:
             MPIRunner *runner_;
+            QHash<int, WorkerStatus> workers_; //!< A map of the workers. The key is the rank of the process.
+
+            WorkerStatus & getFreeWorker(); //!< Get a worker not marked as working.
         };
     }
 }
