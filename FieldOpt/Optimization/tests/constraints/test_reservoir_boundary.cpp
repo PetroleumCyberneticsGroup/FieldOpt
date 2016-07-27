daf6f26dc25f49a38af46d49d6a0cd9c64c09063
Bellout <chakibbb@gmail.com>
2016-07-27 12:40:42 +0200

    Instantiate boundary_box object using settings stored in optimizer_.

--- FieldOpt/Optimization/tests/constraints/test_reservoir_boundary.cpp
+++ FieldOpt/Optimization/tests/constraints/test_reservoir_boundary.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
 #include "constraints/reservoir_boundary.h"
 #include "Optimization/tests/test_resource_cases.h"
+#include "Optimization/tests/test_resource_optimizer.h"
 #include "Reservoir/tests/test_resource_grids.h"
-#include "Utilities/tests/test_resource_settings.h"
 
 namespace {
 
@@ -12,38 +12,44 @@ namespace {
                                   public TestResources::TestResourceSettings {
 
     public:
-        ReservoirBoundaryTest() { }
+        ReservoirBoundaryTest() {
+
+            bound_settings_.type = Utilities::Settings::Optimizer::ConstraintType::ReservoirBoundary;
+            bound_settings_.well = settings_optimizer_->constraints()[5].wells[1]; // TESTW
+            bound_settings_.box_imin = settings_optimizer_->constraints()[5].box_imin;
+            bound_settings_.box_imax = settings_optimizer_->constraints()[5].box_imax;
+            bound_settings_.box_jmin = settings_optimizer_->constraints()[5].box_jmin;
+            bound_settings_.box_jmax = settings_optimizer_->constraints()[5].box_jmax;
+            bound_settings_.box_kmin = settings_optimizer_->constraints()[5].box_kmin;
+            bound_settings_.box_kmax = settings_optimizer_->constraints()[5].box_kmax;
+
+            boundary_constraint_ = new Optimization::Constraints::ReservoirBoundary(
+                    bound_settings_, variable_property_container_, grid_5spot_);
+        }
+
+        Utilities::Settings::Optimizer::Constraint bound_settings_;
+        Optimization::Constraints::ReservoirBoundary *boundary_constraint_;
+
         virtual ~ReservoirBoundaryTest() { }
         virtual void TearDown() { }
         virtual void SetUp() { }
+
     };
 
     TEST_F(ReservoirBoundaryTest, Initialization) {
-        auto test_boundary = Optimization::Constraints::ReservoirBoundary(
-            constraint_settings_reservoir_boundary_,
-            variable_property_container_, grid_5spot_);
-        EXPECT_FALSE(test_boundary.CaseSatisfiesConstraint(test_case_5_));
-        test_boundary.SnapCaseToConstraints(test_case_5_);
-        EXPECT_TRUE(test_boundary.CaseSatisfiesConstraint(test_case_5_));
+        EXPECT_FALSE(boundary_constraint_->CaseSatisfiesConstraint(test_case_5_));
+        boundary_constraint_->SnapCaseToConstraints(test_case_5_);
+        EXPECT_TRUE(boundary_constraint_->CaseSatisfiesConstraint(test_case_5_));
     }
 
-    // std::cout << "i=" << i << " j=" << j << " k=" << k << std::endl;
-    // std::cout << grid_->GetCell(i, j, k).global_index() << std::endl;
-
    TEST_F(ReservoirBoundaryTest, CheckListBoxEdgeCells) {
-        auto test_boundary = Optimization::Constraints::ReservoirBoundary(
-           constraint_settings_reservoir_boundary_,
-           variable_property_container_, grid_5spot_);
 
-//        constraint_settings_reservoir_boundary_.
-//        std::cout << "well: " << constraint_settings_reservoir_boundary_.well.toStdString();
-
-        auto box_edge_cells_ = test_boundary.returnListOfBoxEdgeCellIndices();
-//        auto box_edge_cells_ = test_boundary.index_list_edge_;
+        auto box_edge_cells_ = boundary_constraint_->returnListOfBoxEdgeCellIndices();
 
         // Debug: verbose level = 4
-        if (settings_full_->verbosity()>3){
-            std::cout << "length (test_reservoir_boundary.cpp):" 
+        int verbosity_level_ = 0;
+        if (verbosity_level_>0){
+            std::cout << "length box_edge_cells_: "
             << box_edge_cells_.length() << std::endl;
         }
 
@@ -53,4 +59,12 @@ namespace {
 //        }
         EXPECT_TRUE(box_edge_cells_.length()>0);
    }
+
+    TEST_F(ReservoirBoundaryTest, CheckBoundConstraintSettings) {
+        // std::cout << "i=" << i << " j=" << j << " k=" << k << std::endl;
+        // std::cout << grid_->GetCell(i, j, k).global_index() << std::endl;
+
+        boundary_constraint_->findCornerCells();
+    }
 }
+
