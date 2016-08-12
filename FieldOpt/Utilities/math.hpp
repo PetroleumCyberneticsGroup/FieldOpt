613ca89114c8bfd5efa5043908c55363d6002130
Einar Baumann <einar.baumann@gmail.com>
2016-08-12 15:47:44 +0200

    Added function to calculate median

--- FieldOpt/Utilities/math.hpp
+++ FieldOpt/Utilities/math.hpp
@@ -10,9 +10,26 @@
  * @return The average value of the elements in the list as a double.
  */
 template<typename T>
-inline double calc_average(QList<T> list) {
+inline double calc_average(const QList<T> list) {
     assert(!list.empty());
     return std::accumulate(list.begin(), list.end(), 0.0) / list.size();
+}
+
+/*!
+ * @brief Calculate the median of a list. The returned value will have the same type as the elements in the list.
+ * If the values are integers and the list has an even number of elements, the result will be floored.
+ * @param list
+ * @return
+ */
+template<typename T>
+inline T calc_median(QList<T> list) {
+    assert(!list.empty());
+    std::sort(list.begin(), list.end());
+    size_t size = list.size();
+    if (size % 2 == 0)
+        return (list[size/2 - 1] + list[size/2])/2;
+    else
+        return list[size/2];
 }
 
 #endif // MATH_FUNCTIONS_H
