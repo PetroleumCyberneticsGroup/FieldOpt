178507fcc10b0be5ac64fc0f7f3f477dc13340cd
Einar Baumann <einar.baumann@gmail.com>
2016-09-09 15:31:39 +0200

    Added license headers

--- FieldOpt/WellIndexCalculator/wellindexcalculator.cpp
+++ FieldOpt/WellIndexCalculator/wellindexcalculator.cpp
@@ -1,3 +1,27 @@
+/******************************************************************************
+   Copyright (C) 2015-2016 Hilmar M. Magnusson <hilmarmag@gmail.com>
+   Modified by Einar J.M. Baumann (2016) <einar.baumann@gmail.com>
+
+   This file and the WellIndexCalculator as a whole is part of the
+   FieldOpt project. However, unlike the rest of FieldOpt, the
+   WellIndexCalculator is provided under the GNU Lesser General Public
+   License.
+
+   WellIndexCalculator is free software: you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version.
+
+   WellIndexCalculator is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with WellIndexCalculator.  If not, see
+   <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include <iostream>
 #include "wellindexcalculator.h"
 
