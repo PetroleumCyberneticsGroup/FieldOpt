ec49f7c8fcef87b8209d055822f69e157cb0b55e
Einar Baumann <einar.baumann@gmail.com>
2016-11-16 12:09:29 +0100

    Added explicit optimizer settings

--- FieldOpt/Optimization/tests/test_resource_optimizer.h
+++ FieldOpt/Optimization/tests/test_resource_optimizer.h
@@ -7,18 +7,47 @@
 
 #include "Model/tests/test_resource_model.h"
 #include "Optimization/case.h"
+#include "test_resource_cases.h"
 
 namespace TestResources {
-    class TestResourceOptimizer : public TestResourceModel {
+    class TestResourceOptimizer : public TestResourceModel, public TestResourceCases {
     protected:
         TestResourceOptimizer() {
             base_case_ = new Optimization::Case(model_->variables()->GetBinaryVariableValues(),
                                                 model_->variables()->GetDiscreteVariableValues(),
                                                 model_->variables()->GetContinousVariableValues());
             base_case_->set_objective_function_value(1000.0);
+
+            settings_compass_search_minimize_unconstrained_ = new Settings::Optimizer(get_json_settings_compass_search_minimize_);
         }
 
         Optimization::Case *base_case_;
+        Settings::Optimizer *settings_compass_search_minimize_unconstrained_;
+
+    private:
+        QJsonObject obj_fun_ {
+                {"Type", "WeightedSum"},
+                {"WeightedSumComponents", QJsonArray{
+                        QJsonObject{
+                                {"Coefficient", 1.0}, {"Property", "CumulativeOilProduction"}, {"TimeStep", -1}, {"IsWellProp", false}
+                        },
+                        QJsonObject{
+                                {"Coefficient", 0.0}, {"Property", "CumulativeWaterProduction"}, {"TimeStep", -1}, {"IsWellProp", false}
+                        }
+                }}
+        };
+        QJsonObject get_json_settings_compass_search_minimize_ {
+                {"Type", "Compass"},
+                {"Mode", "Minimize"},
+                {"Parameters", QJsonObject{
+                        {"MaxEvaluations", 100},
+                        {"InitialStepLength", 0.25},
+                        {"MinimumStepLength", 0.01}
+                }},
+                {"Objective", obj_fun_}
+        };
+
+
     };
 }
 
