547dc191a042538bad31856f0838e34643f61927
bellout <mathias.bellout@ntnu.no>
2017-01-28 15:35:00 +0100

    Added license text

--- FieldOpt/Hdf5SummaryReader/hdf5_summary_reader.cpp
+++ FieldOpt/Hdf5SummaryReader/hdf5_summary_reader.cpp
@@ -1,3 +1,27 @@
+/******************************************************************************
+   Copyright (C) 2015-2016 Einar J.M. Baumann <einar.baumann@gmail.com>
+   Additions by M.Bellout (2017) <mathias.bellout@ntnu.no>
+
+   This file and the WellIndexCalculator as a whole is part of the
+   FieldOpt project. However, unlike the rest of FieldOpt, the
+   WellIndexCalculator is provided under the GNU Lesser General Public
+   License.
+
+   WellIndexCalculator is free software: you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version.
+
+   WellIndexCalculator is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with WellIndexCalculator.  If not, see
+   <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "hdf5_summary_reader.h"
 #include <iostream>
 #include <assert.h>
