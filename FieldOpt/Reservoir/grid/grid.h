c205f8b2f252c5924ba2c20ed21dc4e078033ca0
bellout <mathias.bellout@ntnu.no>
2017-02-03 14:42:49 +0100

    Fixed license header to original format

--- FieldOpt/Reservoir/grid/grid.h
+++ FieldOpt/Reservoir/grid/grid.h
@@ -4,9 +4,9 @@
    This file is part of the FieldOpt project.
 
    FieldOpt is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published
-   by the Free Software Foundation, either version 3 of the License,
-   or (at your option) any later version.
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    FieldOpt is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,6 +16,7 @@
    You should have received a copy of the GNU General Public License
    along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
+
 
 #ifndef GRID_H
 #define GRID_H
