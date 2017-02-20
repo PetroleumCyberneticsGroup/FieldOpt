a9b724f883b51bdab494bf2de5ecdd194fab5a77
bellout <mathias.bellout@ntnu.no>
2017-02-20 22:44:27 +0100

    License, changed test well name from PRODUCER to TESTW.

--- FieldOpt/Model/tests/test_resource_variable_property_container.h
+++ FieldOpt/Model/tests/test_resource_variable_property_container.h
@@ -1,6 +1,21 @@
-//
-// Created by einar on 5/27/16.
-//
+/******************************************************************************
+   Copyright (C) 2015-2016 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 
 #ifndef FIELDOPT_TEST_RESOURCE_VARIABLE_PROPERTY_LIST_H
 #define FIELDOPT_TEST_RESOURCE_VARIABLE_PROPERTY_LIST_H
@@ -20,12 +35,12 @@ namespace TestResources {
             prod_toe_x_ = new ContinousProperty(12);
             prod_toe_y_ = new ContinousProperty(120.1);
             prod_toe_z_ = new ContinousProperty(300.0);
-            prod_heel_x_->setName("SplinePoint#PRODUCER#heel#x");
-            prod_heel_y_->setName("SplinePoint#PRODUCER#heel#y");
-            prod_heel_z_->setName("SplinePoint#PRODUCER#heel#z");
-            prod_toe_x_->setName("SplinePoint#PRODUCER#toe#x");
-            prod_toe_y_->setName("SplinePoint#PRODUCER#toe#y");
-            prod_toe_z_->setName("SplinePoint#PRODUCER#toe#z");
+            prod_heel_x_->setName("SplinePoint#TESTW#heel#x");
+            prod_heel_y_->setName("SplinePoint#TESTW#heel#y");
+            prod_heel_z_->setName("SplinePoint#TESTW#heel#z");
+            prod_toe_x_->setName("SplinePoint#TESTW#toe#x");
+            prod_toe_y_->setName("SplinePoint#TESTW#toe#y");
+            prod_toe_z_->setName("SplinePoint#TESTW#toe#z");
             varcont_prod_spline_ = new VariablePropertyContainer();
             varcont_prod_spline_->AddVariable(prod_heel_x_);
             varcont_prod_spline_->AddVariable(prod_heel_y_);
