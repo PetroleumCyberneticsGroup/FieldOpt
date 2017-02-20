0ff98cc4c0f4237271602fe5c9496d78471bc343
bellout <mathias.bellout@ntnu.no>
2017-02-20 15:17:03 +0100

    Typo indentation + formatting

--- FieldOpt/Optimization/tests/constraints/test_bhp_constraint.cpp
+++ FieldOpt/Optimization/tests/constraints/test_bhp_constraint.cpp
@@ -5,76 +5,78 @@
 
 namespace {
 
-    class BhpConstraintTest : public ::testing::Test, public TestResources::TestResourceOptimizer {
-    protected:
-        BhpConstraintTest() {
-            box_constraint_ = new ::Optimization::Constraints::BhpConstraint(settings_optimizer_->constraints()[0], model_->variables());
-        }
-        virtual ~BhpConstraintTest() {}
-        virtual void SetUp() {}
+class BhpConstraintTest : public ::testing::Test,
+                          public TestResources::TestResourceOptimizer {
+ protected:
+  BhpConstraintTest() {
+      box_constraint_ = new ::Optimization::Constraints::BhpConstraint
+          (settings_optimizer_->constraints()[0], model_->variables());
+  }
+  virtual ~BhpConstraintTest() {}
+  virtual void SetUp() {}
 
-        Optimization::Constraints::BhpConstraint *box_constraint_;
-    };
+  Optimization::Constraints::BhpConstraint *box_constraint_;
+};
 
-    TEST_F(BhpConstraintTest, Constructor) {
-        EXPECT_TRUE(true);
+TEST_F(BhpConstraintTest, Constructor) {
+    EXPECT_TRUE(true);
+}
+
+TEST_F(BhpConstraintTest, BaseCaseSatisfaction) {
+    EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+}
+
+TEST_F(BhpConstraintTest, SatisfactionAfterModification) {
+    // get the bhp variable ids
+    QList<QUuid> bhp_ids;
+    for (auto var : model_->variables()->GetWellBHPVariables("PROD")) {
+        bhp_ids.append(var->id());
     }
 
-    TEST_F(BhpConstraintTest, BaseCaseSatasfaction) {
-        EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+    // set all bhps to 5000 (which exceeds the max value defined, 3000)
+    for (QUuid key : bhp_ids) {
+        base_case_->set_real_variable_value(key, 5000);
+    }
+    EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+
+    // set all bhps to 100 (which is less than the min value defined, 1000)
+    for (QUuid key : bhp_ids) {
+        base_case_->set_real_variable_value(key, 100);
+    }
+    EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+
+    // set all bhps to 1125 (which is within the defined bounds)
+    for (QUuid key : bhp_ids) {
+        base_case_->set_real_variable_value(key, 1125);
+    }
+    EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+}
+
+TEST_F(BhpConstraintTest, SnappingValues) {
+    // get the bhp variable ids
+    QList<QUuid> bhp_ids;
+    for (auto var : model_->variables()->GetWellBHPVariables("PROD")) {
+        bhp_ids.append(var->id());
     }
 
-    TEST_F(BhpConstraintTest, SatisfactionAfterModification) {
-        // get the bhp variable ids
-        QList<QUuid> bhp_ids;
-        for (auto var : model_->variables()->GetWellBHPVariables("PROD")) {
-            bhp_ids.append(var->id());
-        }
-
-        // set all bhps to 5000 (which exceeds the max value defined, 3000)
-        for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 5000);
-        }
-        EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
-
-        // set all bhps to 100 (which is less than the min value defined, 1000)
-        for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 100);
-        }
-        EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
-
-        // set all bhps to 1125 (which is within the defined bounds)
-        for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 1125);
-        }
-        EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+    // set all bhps to 5000 (which exceeds the max value defined, 3000)
+    for (QUuid key : bhp_ids) {
+        base_case_->set_real_variable_value(key, 5000);
     }
+    EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+    // Snap the values back and recheck
+    box_constraint_->SnapCaseToConstraints(base_case_);
+    EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
 
-    TEST_F(BhpConstraintTest, SnappingValues) {
-        // get the bhp variable ids
-        QList<QUuid> bhp_ids;
-        for (auto var : model_->variables()->GetWellBHPVariables("PROD")) {
-            bhp_ids.append(var->id());
-        }
-
-        // set all bhps to 5000 (which exceeds the max value defined, 3000)
-        for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 5000);
-        }
-        EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
-        // Snap the values back and recheck
-        box_constraint_->SnapCaseToConstraints(base_case_);
-        EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
-
-        // set all bhps to 100 (which is less than the min value defined, 1000)
-        for (QUuid key : bhp_ids) {
-            base_case_->set_real_variable_value(key, 100);
-        }
-        EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
-        // Snap the values back and recheck
-        box_constraint_->SnapCaseToConstraints(base_case_);
-        EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+    // set all bhps to 100 (which is less than the min value defined, 1000)
+    for (QUuid key : bhp_ids) {
+        base_case_->set_real_variable_value(key, 100);
     }
+    EXPECT_FALSE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+    // Snap the values back and recheck
+    box_constraint_->SnapCaseToConstraints(base_case_);
+    EXPECT_TRUE(box_constraint_->CaseSatisfiesConstraint(base_case_));
+}
 
 }
 
