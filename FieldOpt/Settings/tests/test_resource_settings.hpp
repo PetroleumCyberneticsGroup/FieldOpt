a9c9bafd0087701293ddc6ade8ea364b7c5ee9a0
bellout <mathias.bellout@ntnu.no>
2017-02-20 23:57:52 +0100

    Removed temporary box constraint settings. These are now read in from driver.json.

--- FieldOpt/Settings/tests/test_resource_settings.hpp
+++ FieldOpt/Settings/tests/test_resource_settings.hpp
@@ -28,8 +28,9 @@ namespace TestResources {
 class TestResourceSettings {
  protected:
   TestResourceSettings() {
-      settings_full_ = new Settings::Settings(ExampleFilePaths::driver_example_,
-                                              ExampleFilePaths::directory_output_);
+      settings_full_ = new Settings::Settings(
+          ExampleFilePaths::driver_example_,
+          ExampleFilePaths::directory_output_);
       settings_optimizer_ = settings_full_->optimizer();
       settings_simulator_ = settings_full_->simulator();
       settings_model_ = settings_full_->model();
@@ -38,16 +39,6 @@ class TestResourceSettings {
 //                                                    "/home/einar/.CLion2016.2/system/cmake/generated/FieldOpt-c9373114/c9373114/Debug/fieldopt_output/");
 //      settings_flow_5spot_->simulator()->set_driver_file_path(TestResources::ExampleFilePaths::flow_drv_5spot_);
 //      settings_flow_5spot_->model()->set_reservoir_grid_path(ExampleFilePaths::grid_5spot_flow_);
-
-      constraint_settings_reservoir_boundary_.type =
-          Settings::Optimizer::ConstraintType::ReservoirBoundary;
-      constraint_settings_reservoir_boundary_.box_imin = 0;
-      constraint_settings_reservoir_boundary_.box_imax = 59;
-      constraint_settings_reservoir_boundary_.box_jmin = 0;
-      constraint_settings_reservoir_boundary_.box_jmax = 59;
-      constraint_settings_reservoir_boundary_.box_kmin = 0;
-      constraint_settings_reservoir_boundary_.box_kmax = 0;
-      constraint_settings_reservoir_boundary_.well = "TESTW";
   }
 
   Settings::Settings *settings_full_;
@@ -55,8 +46,6 @@ class TestResourceSettings {
   Settings::Simulator *settings_simulator_;
   Settings::Model *settings_model_;
 //        Settings::Settings *settings_flow_5spot_;
-
-  Settings::Optimizer::Constraint constraint_settings_reservoir_boundary_;
 };
 
 }
