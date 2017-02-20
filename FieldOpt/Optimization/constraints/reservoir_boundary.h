60dce3966dd59d1e2adfdecfd933602592b966f9
bellout <mathias.bellout@ntnu.no>
2017-02-20 14:27:25 +0100

    description getListOfBoxEdgeCellIndices() function Added function getListOfBoxEdgeCellIndices that uses the limits defining the box constraint to find those cells that make up the edges of the box

--- FieldOpt/Optimization/constraints/reservoir_boundary.h
+++ FieldOpt/Optimization/constraints/reservoir_boundary.h
@@ -6,36 +6,43 @@
 #include "Reservoir/grid/grid.h"
 
 namespace Optimization {
-    namespace Constraints {
+namespace Constraints {
 
-        /*!
-         * \brief The ReservoirBoundary class deals with imposing and checking reservoir boundary constraints
-         *
-         *  The class takes a boundary as input in the form of i,j and k min,max, i.e. a 'box' in
-         *  the grid i,j,k coordinates. It can check wether or not a single well is inside the
-         *  given box domain and, if needed, project the well onto the domain.
-         */
-        class ReservoirBoundary : public Constraint, WellSplineConstraint
-        {
-        public:
-            ReservoirBoundary(const Settings::Optimizer::Constraint &settings,
-                              Model::Properties::VariablePropertyContainer *variables, Reservoir::Grid::Grid *grid);
+/*!
+ * \brief The ReservoirBoundary class deals with
+ * imposing and checking reservoir boundary constraints
+ *
+ *  The class takes a boundary as input in the form of
+ *  i,j and k min,max, i.e. a 'box' in the grid i,j,k
+ *  coordinates. It can check wether or not a single
+ *  well is inside the given box domain and, if needed,
+ *  project the well onto the domain.
+ */
+class ReservoirBoundary : public Constraint, WellSplineConstraint
+{
+ public:
+  ReservoirBoundary(const Settings::Optimizer::Constraint &settings,
+                    Model::Properties::VariablePropertyContainer *variables,
+                    Reservoir::Grid::Grid *grid);
 
-            // Constraint interface
-        public:
-            bool CaseSatisfiesConstraint(Case *c);
-            void SnapCaseToConstraints(Case *c);
+  // Constraint interface
+ public:
+  bool CaseSatisfiesConstraint(Case *c);
+  void SnapCaseToConstraints(Case *c);
 
-        private:
-            int imin_, imax_, jmin_, jmax_, kmin_, kmax_;
-            QList<int> index_list_;
-            Reservoir::Grid::Grid *grid_;
-            Well affected_well_;
-            QList<int> getListOfCellIndices();
+ private:
+  int imin_, imax_, jmin_, jmax_, kmin_, kmax_;
+  QList<int> index_list_;
+  Reservoir::Grid::Grid *grid_;
+  Well affected_well_;
+  QList<int> getListOfCellIndices();
 
-        };
-    }
+  QList<int> getListOfBoxEdgeCellIndices();
+  QList<int> index_list_edge_;
+  QList<int> box_edge_cells_;
+
+};
 }
-
+}
 
 #endif //FIELDOPT_RESERVOIRBOUNDARY_H
