f3d815cf5a66fc7119c3e0ae935816678ac5e25f
Einar Baumann <einar.baumann@gmail.com>
2017-03-21 13:32:34 +0100

    Using old face indices

--- FieldOpt/Reservoir/grid/cell.cpp
+++ FieldOpt/Reservoir/grid/cell.cpp
@@ -21,74 +21,74 @@
 #include <iostream>
 
 namespace Reservoir {
-    namespace Grid {
+namespace Grid {
 
 
-        Cell::Cell(int global_index, IJKCoordinate ijk_index,
-                   double volume, double poro, double permx, double permy, double permz,
-                   Eigen::Vector3d center, std::vector<Eigen::Vector3d> corners)
-        {
-            global_index_ = global_index;
-            ijk_index_ = ijk_index;
-            volume_ = volume;
-            porosity_ = poro;
-            permx_ = permx;
-            permy_ = permy;
-            permz_ = permz;
-            center_ = center;
-            corners_ = corners;
-            initializeFaces();
-        }
+Cell::Cell(int global_index, IJKCoordinate ijk_index,
+           double volume, double poro, double permx, double permy, double permz,
+           Eigen::Vector3d center, std::vector<Eigen::Vector3d> corners)
+{
+    global_index_ = global_index;
+    ijk_index_ = ijk_index;
+    volume_ = volume;
+    porosity_ = poro;
+    permx_ = permx;
+    permy_ = permy;
+    permz_ = permz;
+    center_ = center;
+    corners_ = corners;
+    initializeFaces();
+}
 
-        bool Cell::Equals(const Cell *other) const
-        {
-            return this->global_index() == other->global_index();
-        }
+bool Cell::Equals(const Cell *other) const
+{
+    return this->global_index() == other->global_index();
+}
 
-        bool Cell::Equals(const Cell &other) const
-        {
-            return this->global_index() == other.global_index();
-        }
+bool Cell::Equals(const Cell &other) const
+{
+    return this->global_index() == other.global_index();
+}
 
-        bool Cell::EnvelopsPoint(Eigen::Vector3d point) {
-            bool point_inside = true;
-            for (Face face : faces_) {
-		double dot_prod = (point - face.corners[0]).dot(face.normal_vector);
-		if ( dot_prod < 0)
-                    point_inside = false;
-            }
-            return point_inside;
-        }
-
-        void Cell::initializeFaces() {
-//            int face_indices_points[6][4] = {
-//                     {0, 2, 1, 3},
-//                     {4, 5, 6, 7},
-//                     {0, 4, 2, 6},
-//                     {1, 3, 5, 7},
-//                     {0, 1, 4, 5},
-//                     {2, 6, 3, 7}
-//            };
-//
-
-            int face_indices_points[6][4] = {
-                    {2, 0, 3, 1},
-                    {6, 7, 4, 5},
-                    {2, 6, 0, 4},
-                    {3, 1, 7, 5},
-                    {2, 3, 6, 7},
-                    {0, 4, 1, 5}
-            };
-	    
-            for (int ii = 0; ii < 6; ii++) {               
-		Face face;
-                face.corners.push_back(corners_[face_indices_points[ii][0]]);
-                face.corners.push_back(corners_[face_indices_points[ii][1]]);
-                face.corners.push_back(corners_[face_indices_points[ii][2]]);
-                face.corners.push_back(corners_[face_indices_points[ii][3]]);
-                face.normal_vector = (face.corners[2] - face.corners[0]).cross(face.corners[1] - face.corners[0]).normalized();
-                faces_.push_back(face);
-            }
-        }
+bool Cell::EnvelopsPoint(Eigen::Vector3d point) {
+    bool point_inside = true;
+    for (Face face : faces_) {
+        double dot_prod = (point - face.corners[0]).dot(face.normal_vector);
+        if ( dot_prod < 0)
+            point_inside = false;
     }
+    return point_inside;
+}
+
+void Cell::initializeFaces() {
+            int face_indices_points[6][4] = {
+                     {0, 2, 1, 3},
+                     {4, 5, 6, 7},
+                     {0, 4, 2, 6},
+                     {1, 3, 5, 7},
+                     {0, 1, 4, 5},
+                     {2, 6, 3, 7}
+            };
+
+
+//    int face_indices_points[6][4] = {
+//        {2, 0, 3, 1},
+//        {6, 7, 4, 5},
+//        {2, 6, 0, 4},
+//        {3, 1, 7, 5},
+//        {2, 3, 6, 7},
+//        {0, 4, 1, 5}
+//    };
+
+    for (int ii = 0; ii < 6; ii++) {
+        Face face;
+        face.corners.push_back(corners_[face_indices_points[ii][0]]);
+        face.corners.push_back(corners_[face_indices_points[ii][1]]);
+        face.corners.push_back(corners_[face_indices_points[ii][2]]);
+        face.corners.push_back(corners_[face_indices_points[ii][3]]);
+        face.normal_vector = (face.corners[2] - face.corners[0]).cross(face.corners[1] - face.corners[0]).normalized();
+        faces_.push_back(face);
+    }
+}
+}
 }
