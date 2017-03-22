02431e514a9faaff9eabb05de45bccaec9142166
Einar Baumann <einar.baumann@gmail.com>
2017-03-22 14:37:21 +0100

    Fix indentation

--- FieldOpt/Reservoir/grid/grid.h
+++ FieldOpt/Reservoir/grid/grid.h
@@ -25,90 +25,90 @@
 #include "ERTWrapper/eclgridreader.h"
 
 namespace Reservoir {
-    namespace Grid {
+namespace Grid {
 
-        /*!
-         * \brief The abstract Grid class represents a reservoir grid. It indcludes basic operations for lookup,
-         * checks and calculations on grids.
-         */
-        class Grid
-        {
-        public:
-            /*!
-             * \brief The GridSourceType enum Enumerates the supported grid types.
-             */
-            enum GridSourceType {ECLIPSE};
-            Grid(const Grid& other) = delete;
+/*!
+ * \brief The abstract Grid class represents a reservoir grid. It indcludes basic operations for lookup,
+ * checks and calculations on grids.
+ */
+class Grid
+{
+ public:
+  /*!
+   * \brief The GridSourceType enum Enumerates the supported grid types.
+   */
+  enum GridSourceType {ECLIPSE};
+  Grid(const Grid& other) = delete;
 
-            /*!
-             * \brief The Dims struct Contains the grid dimensions.
-             */
-            struct Dims {
-                int nx; int ny; int nz;
-            };
+  /*!
+   * \brief The Dims struct Contains the grid dimensions.
+   */
+  struct Dims {
+    int nx; int ny; int nz;
+  };
 
-            virtual ~Grid();
+  virtual ~Grid();
 
-            /*!
-             * \brief Dimensions Returns the grid dimensions (number of blocks in x, y and z directions).
-             * \return
-             */
-            virtual Dims Dimensions() = 0;
+  /*!
+   * \brief Dimensions Returns the grid dimensions (number of blocks in x, y and z directions).
+   * \return
+   */
+  virtual Dims Dimensions() = 0;
 
-            /*!
-             * \brief GetCell Get a cell from its global index.
-             */
-            virtual Cell GetCell(int global_index) = 0;
+  /*!
+   * \brief GetCell Get a cell from its global index.
+   */
+  virtual Cell GetCell(int global_index) = 0;
 
-            /*!
-             * \brief GetCell Get a cell from its (i,j,k) index.
-             */
-            virtual Cell GetCell(int i, int j, int k) = 0;
+  /*!
+   * \brief GetCell Get a cell from its (i,j,k) index.
+   */
+  virtual Cell GetCell(int i, int j, int k) = 0;
 
-            /*!
-             * \brief GetCell Get a cell from its (i,j,k) index.
-             */
-            virtual Cell GetCell(IJKCoordinate* ijk) = 0;
+  /*!
+   * \brief GetCell Get a cell from its (i,j,k) index.
+   */
+  virtual Cell GetCell(IJKCoordinate* ijk) = 0;
 
-            /*!
-             * \brief GetBoundingBoxCellIndices Searches for the bounding box of the space
-             * defined by the two point and returns the list of all the cells in that bounding box.
-             */
-            virtual std::vector<int> GetBoundingBoxCellIndices(double xi, double yi, double zi, double xf, double yf, double zf) = 0;
+  /*!
+   * \brief GetBoundingBoxCellIndices Searches for the bounding box of the space
+   * defined by the two point and returns the list of all the cells in that bounding box.
+   */
+  virtual std::vector<int> GetBoundingBoxCellIndices(double xi, double yi, double zi, double xf, double yf, double zf) = 0;
 
-            /*!
-             * \brief GetCellEnvelopingPoint Get the cell enveloping the point (x,y,z) searching the entire grid.
-             * Throws an exception if no cell is found.
-             */
-            virtual Cell GetCellEnvelopingPoint(double x, double y, double z) = 0;
+  /*!
+   * \brief GetCellEnvelopingPoint Get the cell enveloping the point (x,y,z) searching the entire grid.
+   * Throws an exception if no cell is found.
+   */
+  virtual Cell GetCellEnvelopingPoint(double x, double y, double z) = 0;
 
-            /*!
-             * \brief GetCellEnvelopingPoint Get the cell enveloping the point (x,y,z) searching a subsection of the grid.
-             * Throws an exception if no cell is found.
-             */
-            virtual Cell GetCellEnvelopingPoint(double x, double y, double z, std::vector<int> search_set) = 0;
+  /*!
+   * \brief GetCellEnvelopingPoint Get the cell enveloping the point (x,y,z) searching a subsection of the grid.
+   * Throws an exception if no cell is found.
+   */
+  virtual Cell GetCellEnvelopingPoint(double x, double y, double z, std::vector<int> search_set) = 0;
 
-            /*!
-             * \brief GetCellEnvelopingPoint Get the cell enveloping the point (xyz.x(), xyz.y(), xyz.z()) searching the entire grid.
-             * Throws an exception if no cell is found.
-             */
-            virtual Cell GetCellEnvelopingPoint(Eigen::Vector3d xyz) = 0;
+  /*!
+   * \brief GetCellEnvelopingPoint Get the cell enveloping the point (xyz.x(), xyz.y(), xyz.z()) searching the entire grid.
+   * Throws an exception if no cell is found.
+   */
+  virtual Cell GetCellEnvelopingPoint(Eigen::Vector3d xyz) = 0;
 
-            /*!
-             * \brief GetCellEnvelopingPoint Get the cell enveloping the point (xyz.x(), xyz.y(), xyz.z()) searching a subsection of the grid.
-             * Throws an exception if no cell is found.
-             * \param xyz Point to check
-             * \param search_set Set to check first
-             */
-            virtual Cell GetCellEnvelopingPoint(Eigen::Vector3d xyz, std::vector<int> search_set) = 0;
+  /*!
+   * \brief GetCellEnvelopingPoint Get the cell enveloping the point (xyz.x(), xyz.y(), xyz.z()) searching a subsection of the grid.
+   * Throws an exception if no cell is found.
+   * \param xyz Point to check
+   * \param search_set Set to check first
+   */
+  virtual Cell GetCellEnvelopingPoint(Eigen::Vector3d xyz, std::vector<int> search_set) = 0;
 
-        protected:
-            GridSourceType type_;
-            std::string file_path_;
-            Grid(GridSourceType type, std::string file_path);
-        };
+ protected:
+  GridSourceType type_;
+  std::string file_path_;
+  Grid(GridSourceType type, std::string file_path);
+};
 
-    }
+}
 }
 
 
