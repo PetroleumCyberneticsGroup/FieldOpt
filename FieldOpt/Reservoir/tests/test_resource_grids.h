93839de324b31355169890f9d31fcd2ccdcdb6f3
bellout <mathias.bellout@ntnu.no>
2017-03-28 11:07:46 +0200

    auto-indent

--- FieldOpt/Reservoir/tests/test_resource_grids.h
+++ FieldOpt/Reservoir/tests/test_resource_grids.h
@@ -24,16 +24,18 @@
 #include "Reservoir/grid/eclgrid.h"
 
 namespace TestResources {
-    class TestResourceGrids {
-    protected:
-        TestResourceGrids() {
-            grid_horzwel_ = new Reservoir::Grid::ECLGrid("../examples/ECLIPSE/HORZWELL/HORZWELL.EGRID");
-            grid_5spot_ = new Reservoir::Grid::ECLGrid("../examples/ADGPRS/5spot/ECL_5SPOT.EGRID");
-        }
+class TestResourceGrids {
+ protected:
+  TestResourceGrids() {
+    grid_horzwel_ = new Reservoir::Grid::ECLGrid(
+        "../examples/ECLIPSE/HORZWELL/HORZWELL.EGRID");
+    grid_5spot_ = new Reservoir::Grid::ECLGrid(
+        "../examples/ADGPRS/5spot/ECL_5SPOT.EGRID");
+  }
 
-        Reservoir::Grid::Grid *grid_5spot_;
-        Reservoir::Grid::Grid *grid_horzwel_;
-    };
+  Reservoir::Grid::Grid *grid_5spot_;
+  Reservoir::Grid::Grid *grid_horzwel_;
+};
 }
 
 #endif //FIELDOPT_TEST_RESOURCE_RESERVOIRS_H
