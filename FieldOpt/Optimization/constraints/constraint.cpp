6542875c9b076200bc9d507e14e3fd0e4b3f9a4d
Einar Baumann <einar.baumann@gmail.com>
2017-03-28 14:37:27 +0200

    Add methods for bound constraint magic

--- FieldOpt/Optimization/constraints/constraint.cpp
+++ FieldOpt/Optimization/constraints/constraint.cpp
@@ -1,22 +1,45 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 #include "constraint.h"
 
 namespace Optimization {
-    namespace Constraints {
+namespace Constraints {
 
-        Constraint::Constraint() {
-            logging_enabled_ = false;
-        }
+Constraint::Constraint() {
+    logging_enabled_ = false;
+}
 
-        void Constraint::EnableLogging(QString output_directory_path) {
-            logging_enabled_ = true;
-            constraint_log_path_ = output_directory_path + "/log_constraints.txt";
-        }
+void Constraint::EnableLogging(QString output_directory_path) {
+    logging_enabled_ = true;
+    constraint_log_path_ = output_directory_path + "/log_constraints.txt";
+}
 
-        void Constraint::SetVerbosityLevel(int level) {
-            verbosity_level_ = level;
-        }
+void Constraint::SetVerbosityLevel(int level) {
+    verbosity_level_ = level;
+}
+Eigen::VectorXd Constraint::GetLowerBounds() const {
+    throw std::runtime_error("Attempted to get bounds from a non-bound cosntraint.");
+}
+Eigen::VectorXd Constraint::GetUpperBounds() const {
+    throw std::runtime_error("Attempted to get bounds from a non-bound cosntraint.");
+}
 
-
-    }
+}
 }
 
--- FieldOpt/Optimization/constraints/constraint.h
+++ FieldOpt/Optimization/constraints/constraint.h
@@ -1,3 +1,21 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 #ifndef CONSTRAINT_H
 #define CONSTRAINT_H
 
@@ -6,44 +24,64 @@
 #include "Model/properties/variable_property_container.h"
 
 namespace Optimization {
-    namespace Constraints {
+namespace Constraints {
 
 /*!
  * \brief The Constraint class is the abstract parent class to all other constraint classes. One Constraint
  * object should be created for each defined constraint.
  */
-        class Constraint
-        {
-        public:
-            Constraint();
+class Constraint
+{
+ public:
+  Constraint();
 
-            /*!
-             * \brief CaseSatisfiesConstraint checks whether a case satisfies the constraints for all
-             * applicable variables.
-             * \param c The case to be checked.
-             * \return True if the constraint is satisfied; otherwise false.
-             */
-            virtual bool CaseSatisfiesConstraint(Case *c) = 0;
+  /*!
+   * \brief CaseSatisfiesConstraint checks whether a case satisfies the constraints for all
+   * applicable variables.
+   * \param c The case to be checked.
+   * \return True if the constraint is satisfied; otherwise false.
+   */
+  virtual bool CaseSatisfiesConstraint(Case *c) = 0;
 
-            /*!
-             * \brief SnapCaseToConstraints Snaps all variable values in the case to the closest value
-             * that satisfies the constraint.
-             * \param c The case that should have it's variable values snapped.
-             */
-            virtual void SnapCaseToConstraints(Case *c) = 0;
+  /*!
+   * \brief SnapCaseToConstraints Snaps all variable values in the case to the closest value
+   * that satisfies the constraint.
+   * \param c The case that should have it's variable values snapped.
+   */
+  virtual void SnapCaseToConstraints(Case *c) = 0;
 
-            void EnableLogging(QString output_directory_path);
-            void SetVerbosityLevel(int level);
+  void EnableLogging(QString output_directory_path);
+  void SetVerbosityLevel(int level);
 
-        protected:
-            bool logging_enabled_;
-            int verbosity_level_;
+  /*!
+   * @brief Indicates whether the constaint is a bound constraint.
+   *
+   * This should be overridden and return true for all constraints
+   * that are bound constraints _and_ are able to return min and
+   * max bound vectors.
+   * @return True if the constraint is a bound constraint; otherwise false.
+   */
+  virtual bool IsBoundConstraint() const { return false; }
 
-        private:
-            QString constraint_log_path_; //!< Path to the constraint log path to be written.
-        };
+  /*!
+   * @brief Get a vector defining the lower bounds for the variables.
+   */
+  virtual Eigen::VectorXd GetLowerBounds() const;
 
-    }
+  /*!
+   * @brief Get a vector defining the upper bounds for the variables.
+   */
+  virtual Eigen::VectorXd GetUpperBounds() const;
+
+ protected:
+  bool logging_enabled_;
+  int verbosity_level_;
+
+ private:
+  QString constraint_log_path_; //!< Path to the constraint log path to be written.
+};
+
+}
 }
 
 #endif // CONSTRAINT_H
