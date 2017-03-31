8d0efe4490797dc99b7661d442427768ff1dca1c
Einar Baumann <einar.baumann@gmail.com>
2017-03-31 14:26:50 +0200

    Autoindent and license headers

--- FieldOpt/Runner/runners/worker.cpp
+++ FieldOpt/Runner/runners/worker.cpp
@@ -1,33 +1,52 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "worker.h"
 
 namespace Runner {
-    namespace MPI {
+namespace MPI {
 
-        Worker::Worker(MPIRunner *runner) {
-            runner_ = runner;
-            runner_->RecvModelSynchronizationObject();
-            std::cout << "Initialized Worker on " << runner_->world().rank() << std::endl;
-        }
+Worker::Worker(MPIRunner *runner) {
+    runner_ = runner;
+    runner_->RecvModelSynchronizationObject();
+    std::cout << "Initialized Worker on " << runner_->world().rank() << std::endl;
+}
 
-        void Worker::RecvUnevaluatedCase() {
-            auto msg = MPIRunner::Message();
-            msg.source = runner_->scheduler_rank_;
-            msg.tag = MPIRunner::MsgTag::CASE_UNEVAL;
-            runner_->RecvMessage(msg);
-            current_case_ = msg.c;
-        }
+void Worker::RecvUnevaluatedCase() {
+    auto msg = MPIRunner::Message();
+    msg.source = runner_->scheduler_rank_;
+    msg.tag = MPIRunner::MsgTag::CASE_UNEVAL;
+    runner_->RecvMessage(msg);
+    current_case_ = msg.c;
+}
 
-        void Worker::SendEvaluatedCase(MPIRunner::MsgTag tag) {
-            auto msg = MPIRunner::Message();
-            msg.destination = runner_->scheduler_rank_;
-            msg.c = current_case_;
-            msg.tag = MPIRunner::MsgTag::CASE_EVAL_SUCCESS;
-            runner_->SendMessage(msg);
-        }
+void Worker::SendEvaluatedCase(MPIRunner::MsgTag tag) {
+    auto msg = MPIRunner::Message();
+    msg.destination = runner_->scheduler_rank_;
+    msg.c = current_case_;
+    msg.tag = MPIRunner::MsgTag::CASE_EVAL_SUCCESS;
+    runner_->SendMessage(msg);
+}
 
-        Optimization::Case *Worker::GetCurrentCase() {
-            return current_case_;
-        }
+Optimization::Case *Worker::GetCurrentCase() {
+    return current_case_;
+}
 
-    }
+}
 }
--- FieldOpt/Runner/runners/worker.h
+++ FieldOpt/Runner/runners/worker.h
@@ -1,36 +1,55 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef FIELDOPT_WOKER_H
 #define FIELDOPT_WOKER_H
 
 #include "mpi_runner.h"
 
 namespace Runner {
-    namespace MPI {
-        /*!
-         * @brief The Worker class is responsible for receiving and sending from/to an overseer object.
-         * The runner taken as a parameter in the constructor is primarily used for the common MPI helpers.
-         */
-        class Worker {
-        public:
-            Worker(MPIRunner *runner);
+namespace MPI {
+/*!
+ * @brief The Worker class is responsible for receiving and sending from/to an overseer object.
+ * The runner taken as a parameter in the constructor is primarily used for the common MPI helpers.
+ */
+class Worker {
+ public:
+  Worker(MPIRunner *runner);
 
-            /*!
-             * @brief Receive an unevaluated case from the Scheduler and set it as the current_case_.
-             */
-            void RecvUnevaluatedCase();
+  /*!
+   * @brief Receive an unevaluated case from the Scheduler and set it as the current_case_.
+   */
+  void RecvUnevaluatedCase();
 
-            /*!
-             * @brief Send the current_case_ back to the Scheduler.
-             * @param tag The tag the message should be sent with, indicating whether the evaluation was successful.
-             */
-            void SendEvaluatedCase(MPIRunner::MsgTag tag);
+  /*!
+   * @brief Send the current_case_ back to the Scheduler.
+   * @param tag The tag the message should be sent with, indicating whether the evaluation was successful.
+   */
+  void SendEvaluatedCase(MPIRunner::MsgTag tag);
 
-            Optimization::Case *GetCurrentCase();
+  Optimization::Case *GetCurrentCase();
 
-        private:
-            MPIRunner *runner_;
-            Optimization::Case *current_case_;
-        };
-    }
+ private:
+  MPIRunner *runner_;
+  Optimization::Case *current_case_;
+};
+}
 }
 
 #endif //FIELDOPT_WOKER_H
