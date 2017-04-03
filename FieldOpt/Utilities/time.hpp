78f32f446846be60a9fc04e56dcd92d6709eca5c
Einar Baumann <einar.baumann@gmail.com>
2017-04-03 13:39:50 +0200

    Added functions to create time strings

--- FieldOpt/Utilities/time.hpp
+++ FieldOpt/Utilities/time.hpp
@@ -21,4 +21,25 @@ inline int time_since_seconds(const QDateTime t) {
     return time_span_seconds(t, QDateTime::currentDateTime());
 }
 
+/*!
+ * @brief Get a time stamp string formatted as YYYY-MM-ddTHH:mm:ss
+ * @return
+ */
+inline std::string timestamp_string() {
+    return QDateTime::currentDateTime().toString(Qt::ISODate).toStdString();
+}
+
+/*!
+ * @brief Convert seconds to a time span string formatted as HH:mm:ss
+ * @param seconds Time span in seconds to convert.
+ * @return Time span string.
+ */
+inline std::string timespan_string(int seconds) {
+    int hrs = seconds/3600;
+    int mins = (seconds - hrs*3600) / 60;
+    int secs = seconds % 60;
+    std::string tstamp = std::to_string(hrs) + ":" + std::to_string(mins) + ":" + std::to_string(secs);
+    return tstamp;
+}
+
 #endif // TIME_FUNCTIONS_H
