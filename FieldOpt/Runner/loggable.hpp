3ec3126287e07da4ebd487b97e12ecf3da769d05
Einar Baumann <einar.baumann@gmail.com>
2017-04-03 10:49:02 +0200

    Add an abstract class describing a loggable object

---
+++ FieldOpt/Runner/loggable.hpp
@@ -0,0 +1,88 @@
+/******************************************************************************
+   Created by einar on 4/3/17.
+   Copyright (C) 2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+#ifndef FIELDOPT_LOGGABLE_H
+#define FIELDOPT_LOGGABLE_H
+
+#include <string>
+
+using namespace std;
+
+/*!
+ * @brief The abstract Loggable class specifies the methods that must
+ * be implemented by all classes that are to be logged by the Logger class.
+ */
+class Loggable {
+
+ public:
+
+  enum LogTarget {LOG_CASE, LOG_OPTIMIZER, LOG_EXTENDED};
+
+  /*!
+   * @brief Notify the logger that a entry should be written.
+   * @param object The object to be logged.
+   */
+  virtual void NotifyLogger(Loggable object) = 0;
+
+  /*!
+   * @brief Get the target log for the object. E.g. if LOG_CASE is returned,
+   * an entry will be written to the case log when NotifyLogger is called.
+   * @return The target log.
+   */
+  virtual LogTarget GetLogTarget() = 0;
+
+  /*!
+   * @brief Get a string-string map describing the state of the loggable object.
+   *
+   * The keys should be the corresponding column headers (or JSON keys) in
+   * the log, an the value should be the four-character code, e.g.
+   *
+   *    "EvalSt": "OKAY"
+   *
+   * Whitespace padding will be added by the logger.
+   *
+   * @return A string-string map of column headers and state indicators.
+   */
+  virtual map<string, string> GetState() = 0;
+
+  /*!
+   * @brief Get the UUID of the object to be written to the log.
+   */
+  virtual QUuid GetId() = 0;
+
+  /*!
+   * @brief Get a string-vector map containing values to be logged.
+   *
+   * The keys should be the column headers (or JSON keys) in the corresponding log,
+   * and the values should be vectors containing the value(s) to be logged.
+   *
+   * For case logging, string should be the name of the variable and the vector
+   * should be of length one and contain the value of that variable, e.g.
+   *    "PROD#PHP#20": [142.0]
+   *
+   * For production data logs, the key should be the name of the vector, and the
+   * vector should contain the production data, e.g.
+   *    "FOPT": [0.0, 200.0, 240.0]
+   * @return A string-vector map of values.
+   */
+  virtual map<string, vector<double>> GetVaues() = 0;
+
+};
+
+#endif //FIELDOPT_LOGGABLE_H
