2e13d7fd2e07a312aee19ec32e1a0e51f281c97a
Einar Baumann <einar.baumann@gmail.com>
2017-04-06 13:42:55 +0200

    Constraints now have a string name (and autoindent)

--- FieldOpt/Optimization/constraints/rate_constraint.h
+++ FieldOpt/Optimization/constraints/rate_constraint.h
@@ -23,24 +23,26 @@
 #include "constraint.h"
 
 namespace Optimization {
-    namespace Constraints {
-        class RateConstraint : public Constraint {
-        public:
-            RateConstraint(Settings::Optimizer::Constraint settings,
-                           Model::Properties::VariablePropertyContainer *variables);
+namespace Constraints {
+class RateConstraint : public Constraint {
+ public:
+  RateConstraint(Settings::Optimizer::Constraint settings,
+                 Model::Properties::VariablePropertyContainer *variables);
 
-        // Constraint interface
-        public:
-            bool CaseSatisfiesConstraint(Case *c);
-            void SnapCaseToConstraints(Case *c);
+  string name() override { return "RateConstraint"; }
 
-        private:
-            double min_;
-            double max_;
-            QStringList affected_well_names_;
-            QList<Model::Properties::ContinousProperty *> affected_real_variables_;
-        };
-    }
+  // Constraint interface
+ public:
+  bool CaseSatisfiesConstraint(Case *c);
+  void SnapCaseToConstraints(Case *c);
+
+ private:
+  double min_;
+  double max_;
+  QStringList affected_well_names_;
+  QList<Model::Properties::ContinousProperty *> affected_real_variables_;
+};
+}
 }
 
 #endif //FIELDOPT_RATE_CONSTRAINT_H
