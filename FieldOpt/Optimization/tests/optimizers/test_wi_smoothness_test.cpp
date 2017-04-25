31f47c136c268ff7a65d8ab67a0870e1d8e364ec
bellout <mathias.bellout@ntnu.no>
2017-04-25 14:29:31 +0200

    set up test for wi smoothness test

--- FieldOpt/Optimization/tests/optimizers/test_wi_smoothness_test.cpp
+++ FieldOpt/Optimization/tests/optimizers/test_wi_smoothness_test.cpp
@@ -1,4 +1,46 @@
-//
-// Created by bellout on 4/25/17.
-//
+/******************************************************************************
+   Copyright (C) 2017 Mathias C. Bellout <mathias.bellout@ntnu.no>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#include <gtest/gtest.h>
+#include <Runner/tests/test_resource_runner.hpp>
+#include "Optimization/optimizers/compass_search.h"
+#include "Optimization/tests/test_resource_optimizer.h"
+#include "Reservoir/tests/test_resource_grids.h"
+#include "Optimization/tests/test_resource_test_functions.h"
+
+using namespace TestResources::TestFunctions;
+using namespace Optimization::Optimizers;
+
+namespace {
+
+class WISmoothnessTest : public ::testing::Test,
+                         public TestResources::TestResourceOptimizer,
+                         public TestResources::TestResourceGrids {
+ protected:
+  WISmoothnessTest() {
+      base_ = base_case_;
+  }
+  virtual ~WISmoothnessTest() {}
+  virtual void SetUp() {}
+
+  Optimization::Case *base_;
+
+};
+}
 
