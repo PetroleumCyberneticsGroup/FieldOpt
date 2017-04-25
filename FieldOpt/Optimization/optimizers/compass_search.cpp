b6389969b2cb69dfefe3e30cc1a881f92e26eabd
bellout <mathias.bellout@ntnu.no>
2017-04-25 12:09:39 +0200

    auto indent + eb license

--- FieldOpt/Optimization/optimizers/compass_search.cpp
+++ FieldOpt/Optimization/optimizers/compass_search.cpp
@@ -1,63 +1,83 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include <iostream>
 #include "compass_search.h"
 #include "gss_patterns.hpp"
 
 namespace Optimization {
-    namespace Optimizers {
+namespace Optimizers {
 
-        CompassSearch::CompassSearch(Settings::Optimizer *settings,
-                                     Case *base_case,
-                                     Model::Properties::VariablePropertyContainer *variables,
-                                     Reservoir::Grid::Grid *grid,
-                                     Logger *logger
-        )
-                : GSS(settings, base_case, variables, grid, logger)
-        {
-            directions_ = GSSPatterns::Compass(num_vars_);
-            step_lengths_ = Eigen::VectorXd(directions_.size());
-            step_lengths_.fill(settings->parameters().initial_step_length);
-        }
+CompassSearch::CompassSearch(
+    Settings::Optimizer *settings,
+    Case *base_case,
+    Model::Properties::VariablePropertyContainer *variables,
+    Reservoir::Grid::Grid *grid,
+    Logger *logger
+)
+    : GSS(settings, base_case, variables, grid, logger)
+{
+    directions_ = GSSPatterns::Compass(num_vars_);
+    step_lengths_ = Eigen::VectorXd(directions_.size());
+    step_lengths_.fill(settings->parameters().initial_step_length);
+}
 
-        void CompassSearch::iterate()
-        {
-            if (!is_successful_iteration() && iteration_ != 0)
-                contract();
-            case_handler_->AddNewCases(generate_trial_points());
-            case_handler_->ClearRecentlyEvaluatedCases();
-            iteration_++;
-        }
+void CompassSearch::iterate()
+{
+    if (!is_successful_iteration() && iteration_ != 0)
+        contract();
+    case_handler_->AddNewCases(generate_trial_points());
+    case_handler_->ClearRecentlyEvaluatedCases();
+    iteration_++;
+}
 
-        QString CompassSearch::GetStatusStringHeader() const
-        {
-            return QString("%1,%2,%3,%4,%5,%6,%7")
-                    .arg("Iteration")
-                    .arg("EvaluatedCases")
-                    .arg("QueuedCases")
-                    .arg("RecentlyEvaluatedCases")
-                    .arg("TentativeBestCaseID")
-                    .arg("TentativeBestCaseOFValue")
-                    .arg("StepLength");
-        }
+QString CompassSearch::GetStatusStringHeader() const
+{
+    return QString("%1,%2,%3,%4,%5,%6,%7")
+        .arg("Iteration")
+        .arg("EvaluatedCases")
+        .arg("QueuedCases")
+        .arg("RecentlyEvaluatedCases")
+        .arg("TentativeBestCaseID")
+        .arg("TentativeBestCaseOFValue")
+        .arg("StepLength");
+}
 
-        QString CompassSearch::GetStatusString() const
-        {
-            return QString("%1,%2,%3,%4,%5,%6,%7")
-                    .arg(iteration_)
-                    .arg(nr_evaluated_cases())
-                    .arg(nr_queued_cases())
-                    .arg(nr_recently_evaluated_cases())
-                    .arg(GetTentativeBestCase()->id().toString())
-                    .arg(GetTentativeBestCase()->objective_function_value())
-                    .arg(step_lengths_[0]);
-        }
+QString CompassSearch::GetStatusString() const
+{
+    return QString("%1,%2,%3,%4,%5,%6,%7")
+        .arg(iteration_)
+        .arg(nr_evaluated_cases())
+        .arg(nr_queued_cases())
+        .arg(nr_recently_evaluated_cases())
+        .arg(GetTentativeBestCase()->id().toString())
+        .arg(GetTentativeBestCase()->objective_function_value())
+        .arg(step_lengths_[0]);
+}
 
-        void CompassSearch::handleEvaluatedCase(Case *c) {
-            if (isImprovement(c))
-                updateTentativeBestCase(c);
-        }
+void CompassSearch::handleEvaluatedCase(Case *c) {
+    if (isImprovement(c))
+        updateTentativeBestCase(c);
+}
 
-        bool CompassSearch::is_successful_iteration() {
-            return case_handler_->RecentlyEvaluatedCases().contains(GetTentativeBestCase());
-        }
+bool CompassSearch::is_successful_iteration() {
+    return case_handler_->RecentlyEvaluatedCases().contains(GetTentativeBestCase());
+}
 
-    }}
+}}
--- FieldOpt/Optimization/optimizers/compass_search.h
+++ FieldOpt/Optimization/optimizers/compass_search.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef COMPASSSEARCH_H
 #define COMPASSSEARCH_H
 
@@ -30,8 +49,7 @@ class CompassSearch : public GSS
                 Case *base_case,
                 ::Model::Properties::VariablePropertyContainer *variables,
                 Reservoir::Grid::Grid *grid,
-                Logger *logger
-  );
+                Logger *logger);
 
   QString GetStatusStringHeader() const;
   QString GetStatusString() const;
@@ -40,13 +58,13 @@ class CompassSearch : public GSS
 
   //!< Step or contract, perturb, and clear list of
   //recently evaluated cases.
-  void iterate(); 
+  void iterate();
 
 
   //!< Check if this iteration was successful (i.e. if
   //the current tent. best case was found in this
   //iteration).
-  bool is_successful_iteration(); 
+  bool is_successful_iteration();
 
  protected:
   void handleEvaluatedCase(Case *c) override;
