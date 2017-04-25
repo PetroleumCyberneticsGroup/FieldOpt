3bc793b8b63eb7b0d463341808ab4f2d00bdbfb0
bellout <mathias.bellout@ntnu.no>
2017-04-25 11:31:58 +0200

    auto indent compass_search.h

--- FieldOpt/Optimization/optimizers/compass_search.h
+++ FieldOpt/Optimization/optimizers/compass_search.h
@@ -5,41 +5,53 @@
 #include "GSS.h"
 
 namespace Optimization {
-    namespace Optimizers {
+namespace Optimizers {
 
 /*!
- * \brief The CompassSearch class is an implementation of the Compass Search optimization algorithm
- * described by Torczon, Lewis and Kolda. It is an extension of the abstract GSS class.
+ * \brief The CompassSearch class is an implementation
+ * of the Compass Search optimization algorithm
+ * described by Torczon, Lewis and Kolda. It is an
+ * extension of the abstract GSS class.
  *
- * This algorithm only supports integer and real variables, and not both at the same time.
+ * This algorithm only supports integer and real
+ * variables, and not both at the same time.
  *
  * Reference:
  *
- * Kolda, Tamara G., Robert Michael Lewis, and Virginia Torczon.
- *  "Optimization by direct search: New perspectives on some classical and modern methods."
- *  SIAM review 45.3 (2003): 385-482.
+ * Kolda, Tamara G., Robert Michael Lewis, and
+ *  Virginia Torczon. "Optimization by direct search:
+ *  New perspectives on some classical and modern
+ *  methods." SIAM review 45.3 (2003): 385-482.
  */
-        class CompassSearch : public GSS
-        {
-        public:
-            CompassSearch(::Settings::Optimizer *settings,
-                          Case *base_case,
-                          ::Model::Properties::VariablePropertyContainer *variables,
-                          Reservoir::Grid::Grid *grid,
-                          Logger *logger
-            );
+class CompassSearch : public GSS
+{
+ public:
+  CompassSearch(::Settings::Optimizer *settings,
+                Case *base_case,
+                ::Model::Properties::VariablePropertyContainer *variables,
+                Reservoir::Grid::Grid *grid,
+                Logger *logger
+  );
 
-            QString GetStatusStringHeader() const;
-            QString GetStatusString() const;
+  QString GetStatusStringHeader() const;
+  QString GetStatusString() const;
 
-        private:
-            void iterate(); //!< Step or contract, perturb, and clear list of recently evaluated cases.
-            bool is_successful_iteration(); //!< Check if this iteration was successful (i.e. if the current tent. best case was found in this iteration).
+ private:
 
-        protected:
-            void handleEvaluatedCase(Case *c) override;
-        };
+  //!< Step or contract, perturb, and clear list of
+  //recently evaluated cases.
+  void iterate(); 
 
-    }}
+
+  //!< Check if this iteration was successful (i.e. if
+  //the current tent. best case was found in this
+  //iteration).
+  bool is_successful_iteration(); 
+
+ protected:
+  void handleEvaluatedCase(Case *c) override;
+};
+
+}}
 
 #endif // COMPASSSEARCH_H
