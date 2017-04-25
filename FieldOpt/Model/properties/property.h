853905e95fa792dd37cfd6445705f85c60baf3ab
bellout <mathias.bellout@ntnu.no>
2017-04-25 23:59:11 +0200

    formatting, eb license header, auto indent

--- FieldOpt/Model/properties/property.h
+++ FieldOpt/Model/properties/property.h
@@ -1,27 +1,23 @@
 /******************************************************************************
- *
- * variable.h
- *
- * Created: 22.09.2015 2015 by einar
- *
- * This file is part of the FieldOpt project.
- *
- * Copyright (C) 2015-2015 Einar J.M. Baumann <einar.baumann@ntnu.no>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *****************************************************************************/
+   Created: 22.09.2015 2015 by einar
+
+   Copyright (C) 2015-2016 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 
 #ifndef PROPERTY_H
 #define PROPERTY_H
@@ -34,93 +30,138 @@ namespace Model {
 namespace Properties {
 
 /*!
- * \brief The Property class is an abstract class implemented by
- * specific property types, i.e. integer, real and binary. It holds
- * and to some extends describes the value of a property in the model.
+ * \brief The Property class is an abstract class
+ * implemented by specific property types, i.e.
+ * integer, real and binary. It holds and to some
+ * extends describes the value of a property in
+ * the model.
  */
 class Property
 {
-public:
-    enum Type { Discrete, Continous, Binary }; //!< The underlying datatype of the property's value.
+ public:
 
-    Type type() const { return type_; } //!< Get the underlying datatype of the property's value.
-    QString name() const { return name_;} //!< Get the name set for the variable. Returns an empty string if name has not been set.
-    void setName(QString name) { name_ = name; } //!< Set the name of the variable.
+  //!< The underlying datatype of the property's value.
+  enum Type { Discrete, Continous, Binary };
 
-    bool IsLocked() const { return locked_; } //!< Check if the property is locked.
-    void Lock() { locked_ = true; } //!< Set the property to locked. The value of a locked property cannot be changed.
-    void Unlock() { locked_ = false; } //!< Unlock a property.
+  //!< Get the underlying datatype of the property's value.
+  Type type() const { return type_; }
 
-    /*!
-     * \brief //!< Set the property to variable.
-     *
-     * This will also populate the info data structure for the property.
-     * The property name is used to create the Info datastructure, thus the name of the property
-     * must be set before setting it to variable.
-     */
-    void SetVariable();
-    bool isVariable() { return is_variable_; } //!< Check if the property is variable.
+  //!< Get the name set for the variable. Returns
+  // an empty string if name has not been set.
+  QString name() const { return name_;}
 
-    QUuid id() const { return id_; } //!< Unique ID for the property.
+  //!< Set the name of the variable.
+  void setName(QString name) { name_ = name; }
 
-    /*!
-     * @brief Update the UUID for this object. This is used when synchronizing models between process instances when
-     * using MPI.
-     * @param new_id The new ID for this object.
-     */
-    void UpdateId(QUuid new_id);
+  //!< Check if the property is locked.
+  bool IsLocked() const { return locked_; }
 
-    /*!
-     * \brief The type of property represented. This type decides which field in the
-     * Info datastructure is set.
-     */
-    enum PropertyType : int {BHP=2001, Rate=2002, SplinePoint=2003, WellBlock=2004, Transmissibility=2005};
+  //!< Set the property to locked. The value
+  // of a locked property cannot be changed.
+  void Lock() { locked_ = true; }
 
-    /*!
-     * \brief For SplinePoint type properties, this is used to indicate whether the property
-     * is for the heel or the toe of the well.
-     */
-    enum SplineEnd : int {Heel=3001, Toe=3002};
+  //!< Unlock a property.
+  void Unlock() { locked_ = false; }
 
-    /*!
-     * \brief For SplinePoint and WellBlock type variables, this indicates which coordinate
-     * the property is for.
-     */
-    enum Coordinate : int {x=4001, y=4002, z=4003, i=4004, j=4005, k=4006};
+  /*!
+   * \brief Set the property to variable.
+   *
+   * This will also populate the info data structure
+   * for the property. The property name is used to
+   * create the Info datastructure, thus the name of
+   * the property must be set before setting it to
+   * variable.
+   */
+  void SetVariable();
 
-    /*!
-     * \brief The PropertyInfo struct contains information about what the property represents.
-     *
-     * This struct is only populated if the property is set variable.
-     */
-    struct PropertyInfo {
-        bool is_set_ = false; //!< Indicates whether the info has been set or not.
-        PropertyType prop_type; //!< The type of property this is (part) of.
-        QString parent_well_name; //!< The name of the well this property belongs to.
-        int index; //!< The number of the well block this property belongs to, or the time step of a control.
-        SplineEnd spline_end;
-        Coordinate coord;
-    };
+  //!< Check if the property is variable.
+  bool isVariable() { return is_variable_; }
 
-    PropertyInfo propertyInfo() const; //!< Get an Info struc containing information about the variable. This is only set if the property is variable.
+  //!< Unique ID for the property.
+  QUuid id() const { return id_; }
 
-protected:
-    Property(Type type) { type_ = type; locked_ = false; is_variable_ = false; name_ = "";}
+  /*!
+   * @brief Update the UUID for this object. This
+   * is used when synchronizing models between
+   * process instances when using MPI.
+   *
+   * @param new_id The new ID for this object.
+   */
+  void UpdateId(QUuid new_id);
 
-private:
-    QUuid id_ = QUuid::createUuid();
-    Type type_;
-    bool locked_;
-    bool is_variable_;
-    QString name_;
-    PropertyInfo info_;
+  /*!
+   * \brief The type of property represented.
+   * This type decides which field in the Info
+   * datastructure is set.
+   */
+  enum PropertyType : int {
+    BHP=2001, Rate=2002, SplinePoint=2003,
+    WellBlock=2004, Transmissibility=2005};
 
-    void set_property_info();
-    PropertyType get_prop_type_name(const QString prop_name) const;
-    QString get_well_name(const QString prop_name) const;
-    SplineEnd get_spline_end(const QString prop_name) const;
-    int get_prop_index(const QString prop_name) const;
-    Coordinate get_prop_coord(const QString prop_name) const;
+  /*!
+   * \brief For SplinePoint type properties,
+   * this is used to indicate whether the property
+   * is for the heel or the toe of the well.
+   */
+  enum SplineEnd : int {Heel=3001, Toe=3002};
+
+  /*!
+   * \brief For SplinePoint and WellBlock type
+   * variables, this indicates which coordinate
+   * the property is for.
+   */
+  enum Coordinate : int {
+    x=4001, y=4002, z=4003,
+    i=4004, j=4005, k=4006};
+
+  /*!
+   * \brief The PropertyInfo struct contains
+   * information about what the property
+   * represents.
+   *
+   * This struct is only populated if the
+   * property is set variable.
+   */
+  struct PropertyInfo {
+    //!< Indicates whether the info has been set or not.
+    bool is_set_ = false;
+    //!< The type of property this is (part) of.
+    PropertyType prop_type;
+    //!< The name of the well this property belongs to.
+    QString parent_well_name;
+    //!< The number of the well block this property
+    // belongs to, or the time step of a control.
+    int index;
+    SplineEnd spline_end;
+    Coordinate coord;
+  };
+
+  //!< Get an Info struc containing information
+  // about the variable. This is only set if
+  // the property is variable.
+  PropertyInfo propertyInfo() const;
+
+ protected:
+  Property(Type type) {
+      type_ = type;
+      locked_ = false;
+      is_variable_ = false;
+      name_ = "";}
+
+ private:
+  QUuid id_ = QUuid::createUuid();
+  Type type_;
+  bool locked_;
+  bool is_variable_;
+  QString name_;
+  PropertyInfo info_;
+
+  void set_property_info();
+  PropertyType get_prop_type_name(const QString prop_name) const;
+  QString get_well_name(const QString prop_name) const;
+  SplineEnd get_spline_end(const QString prop_name) const;
+  int get_prop_index(const QString prop_name) const;
+  Coordinate get_prop_coord(const QString prop_name) const;
 };
 
 }
