b8b83b2b9a8f2702362ff09ee3173d63344f67cb
bellout <mathias.bellout@ntnu.no>
2017-04-27 14:56:43 +0200

    auto indent, eb license, formatting

--- FieldOpt/Optimization/constraints/well_spline_constraint.cpp
+++ FieldOpt/Optimization/constraints/well_spline_constraint.cpp
@@ -1,40 +1,76 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "well_spline_constraint.h"
 #include <boost/lexical_cast.hpp>
 
 namespace Optimization {
-    namespace Constraints {
+namespace Constraints {
 
-        WellSplineConstraint::Well WellSplineConstraint::initializeWell(QList<Model::Properties::ContinousProperty *> vars) {
-            if (vars.length() != 6)
-                throw std::runtime_error("Incorrect number of variables (" + boost::lexical_cast<std::string>(vars.length())
-                                         + ")passed to the initialize well method.");
-
-            Well well;
-            for (auto var : vars) {
-                QStringList nmcomps = var->name().split("#"); // Should be {SplinePoint, WELLNAME, heel/toe, x/y/z}
-                if (QString::compare("heel", nmcomps[2]) == 0) {
-                    if (QString::compare("x", nmcomps[3]) == 0)
-                        well.heel.x = var->id();
-                    else if (QString::compare("y", nmcomps[3]) == 0)
-                        well.heel.y = var->id();
-                    else if (QString::compare("z", nmcomps[3]) == 0)
-                        well.heel.z = var->id();
-                    else throw std::runtime_error("Unable to parse variable " + var->name().toStdString());
-                }
-                else if (QString::compare("toe", nmcomps[2]) == 0) {
-                    if (QString::compare("x", nmcomps[3]) == 0)
-                        well.toe.x = var->id();
-                    else if (QString::compare("y", nmcomps[3]) == 0)
-                        well.toe.y = var->id();
-                    else if (QString::compare("z", nmcomps[3]) == 0)
-                        well.toe.z = var->id();
-                    else throw std::runtime_error("Unable to parse variable " + var->name().toStdString());
-                }
-                else throw std::runtime_error("Unable to parse variable " + var->name().toStdString());
-            }
-            return well;
-        }
-
-
+WellSplineConstraint::Well WellSplineConstraint::
+initializeWell(QList<Model::Properties::ContinousProperty *> vars) {
+    if (vars.length() != 6){
+        throw std::runtime_error(
+            "Incorrect number of variables ("
+                + boost::lexical_cast<std::string>(vars.length())
+                + ") passed to the initialize well method.");
     }
+
+    Well well;
+    for (auto var : vars) {
+        // Should be {SplinePoint, WELLNAME, heel/toe, x/y/z}
+        QStringList nmcomps = var->name().split("#");
+
+        if (QString::compare("heel", nmcomps[2]) == 0) {
+            if (QString::compare("x", nmcomps[3]) == 0)
+                well.heel.x = var->id();
+            else if (QString::compare("y", nmcomps[3]) == 0)
+                well.heel.y = var->id();
+            else if (QString::compare("z", nmcomps[3]) == 0)
+                well.heel.z = var->id();
+            else {
+                throw std::runtime_error(
+                    "Unable to parse variable "
+                        + var->name().toStdString());
+            }
+        }
+        else if (QString::compare("toe", nmcomps[2]) == 0) {
+            if (QString::compare("x", nmcomps[3]) == 0)
+                well.toe.x = var->id();
+            else if (QString::compare("y", nmcomps[3]) == 0)
+                well.toe.y = var->id();
+            else if (QString::compare("z", nmcomps[3]) == 0)
+                well.toe.z = var->id();
+            else {
+                throw std::runtime_error(
+                    "Unable to parse variable "
+                        + var->name().toStdString());
+            }
+        }
+        else {
+            throw std::runtime_error(
+                "Unable to parse variable "
+                    + var->name().toStdString());
+        }
+    }
+    return well;
+}
+
+}
 }
--- FieldOpt/Optimization/constraints/well_spline_constraint.h
+++ FieldOpt/Optimization/constraints/well_spline_constraint.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef WELLSPLINECONSTRAINT_H
 #define WELLSPLINECONSTRAINT_H
 
@@ -5,48 +24,50 @@
 #include "Model/properties/variable_property_container.h"
 
 namespace Optimization {
-    namespace Constraints {
+namespace Constraints {
 
-        /*!
-         * \brief The WellSplineConstraint class acts as a parent class for constraints dealing with well splines.
-         *
-         * This class provides datastructures with initialization methods that ease the application of well spline
-         * constraints.
-         */
-        class WellSplineConstraint
-        {
-        protected:
-            WellSplineConstraint() {}
+/*!
+ * \brief The WellSplineConstraint class acts as a parent
+ * class for constraints dealing with well splines.
+ *
+ * This class provides datastructures with initialization
+ * methods that ease the application of well spline constraints.
+ */
+class WellSplineConstraint
+{
+ protected:
+  WellSplineConstraint() {}
 
-            /*!
-             * \brief The Coord struct hodlds the UUIDs for the variables containing coordinate
-             * values of a WellSpline point.
-             */
-            struct Coord {
-                QUuid x;
-                QUuid y;
-                QUuid z;
-            };
+  /*!
+   * \brief The Coord struct holds the UUIDs for the variables
+   * containing coordinate values of a WellSpline point.
+   */
+  struct Coord {
+    QUuid x;
+    QUuid y;
+    QUuid z;
+  };
 
-            /*!
-             * \brief The Well struct Holds the heel and toe coordinates for a well defined by
-             * a WellSpline.
-             */
-            struct Well {
-                Coord heel;
-                Coord toe;
-            };
+  /*!
+   * \brief The Well struct holds the heel and toe
+   * coordinates for a well defined by a WellSpline.
+   */
+  struct Well {
+    Coord heel;
+    Coord toe;
+  };
 
-            /*!
-             * \brief initializeWell Initialize the Well and Coord datastructures using the provided
-             * variables.
-             * \param vars the six variables defining the spline for a well.
-             */
-            Well initializeWell(QList<Model::Properties::ContinousProperty *> vars);
+  /*!
+   * \brief initializeWell Initialize the Well and Coord
+   * datastructures using the provided variables.
+   * \param vars the six variables defining the spline
+   * for a well.
+   */
+  Well initializeWell(QList<Model::Properties::ContinousProperty *> vars);
 
-        };
+};
 
-    }
+}
 }
 
 #endif // WELLSPLINECONSTRAINT_H
