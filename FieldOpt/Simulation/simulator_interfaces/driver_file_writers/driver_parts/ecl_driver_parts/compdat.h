bbb6f2010b68c1022ec88776d64201230ba3385c
bellout <mathias.bellout@ntnu.no>
2017-04-28 13:26:28 +0200

    eb license, auto indent

--- FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/ecl_driver_parts/compdat.h
+++ FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/ecl_driver_parts/compdat.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef COMPDAT_H
 #define COMPDAT_H
 
@@ -5,17 +24,20 @@
 #include "Model/wells/well.h"
 
 namespace Simulation {
-    namespace SimulatorInterfaces {
-        namespace DriverFileWriters {
-            namespace DriverParts {
-                namespace ECLDriverParts {
+namespace SimulatorInterfaces {
+namespace DriverFileWriters {
+namespace DriverParts {
+namespace ECLDriverParts {
 
 /*!
- * \brief The Compdat class Generates the string for the WELSPECS section in the ECLIPSE simulator.
+ * \brief The Compdat class Generates the string for the
+ * WELSPECS section in the ECLIPSE simulator.
  *
- * The information is taken from both the Model::Wells::Well and Model::Wells::Wellbore::WellBlock classes.
+ * The information is taken from both the Model::Wells::Well
+ * and Model::Wells::Wellbore::WellBlock classes.
  *
- * \todo It is likely that this object has to be deleted and recreated whenever the model changes to get the new settings.
+ * \todo It is likely that this object has to be deleted and
+ * recreated whenever the model changes to get the new settings.
  *
  * \todo Support WELSPECL.
  *
@@ -23,20 +45,19 @@ namespace Simulation {
  *
  * \todo Support spline-defined wells.
  */
-                    class Compdat : public ECLDriverPart
-                    {
-                    public:
-                        Compdat(QList<Model::Wells::Well *> *wells);
-                        QString GetPartString();
+class Compdat : public ECLDriverPart {
+ public:
+  Compdat(QList<Model::Wells::Well *> *wells);
+  QString GetPartString();
 
-                    private:
-                        QList<QStringList> createWellEntries(Model::Wells::Well *well);
-                        QStringList createBlockEntry(QString well_name, double wellbore_radius, Model::Wells::Wellbore::WellBlock *well_block);
-                    };
+ private:
+  QList<QStringList> createWellEntries(Model::Wells::Well *well);
+  QStringList createBlockEntry(QString well_name, double wellbore_radius, Model::Wells::Wellbore::WellBlock *well_block);
+};
 
-                }
-            }
-        }
-    }
+}
+}
+}
+}
 }
 #endif // COMPDAT_H
