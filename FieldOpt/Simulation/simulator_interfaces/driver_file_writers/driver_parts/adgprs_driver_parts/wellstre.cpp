2117cc4b862b2223c45cf1c2dcad72efa88ba1a4
bellout <mathias.bellout@ntnu.no>
2017-04-28 14:22:34 +0200

    formatting

--- FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/adgprs_driver_parts/wellstre.cpp
+++ FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/adgprs_driver_parts/wellstre.cpp
@@ -1,3 +1,22 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #include "wellstre.h"
 
 namespace Simulation {
@@ -6,7 +25,8 @@ namespace DriverFileWriters {
 namespace DriverParts {
 namespace AdgprsDriverParts {
 
-Wellstre::Wellstre(QList<Model::Wells::Well *> *wells, Settings::Simulator::SimulatorFluidModel fluid_model)
+Wellstre::Wellstre(QList<Model::Wells::Well *> *wells,
+                   Settings::Simulator::SimulatorFluidModel fluid_model)
 {
     wells_ = wells;
     fluid_model_ = fluid_model;
@@ -32,7 +52,8 @@ QString Wellstre::createKeyword()
 
 QString Wellstre::createWellEntry(Model::Wells::Well *well)
 {
-    if (well->type() != Settings::Model::WellType::Injector) // Return an empty string if the well is not an injector
+    // Return an empty string if the well is not an injector
+    if (well->type() != Settings::Model::WellType::Injector)
         return "";
 
     if (fluid_model_ == Settings::Simulator::SimulatorFluidModel::BlackOil) {
@@ -43,18 +64,18 @@ QString Wellstre::createWellEntry(Model::Wells::Well *well)
         base_entry_line_[3] = "0";
 
         switch (well->preferred_phase()) {
-        case Settings::Model::PreferredPhase::Water:
-            base_entry_line_[3] = "1";
-            break;
-        case Settings::Model::PreferredPhase::Gas:
-            base_entry_line_[1] = "1";
-            break;
-        case Settings::Model::PreferredPhase::Oil:
-            base_entry_line_[2] = "1";
-            break;
-        default:
-            base_entry_line_[3] = "1";
-            break;
+            case Settings::Model::PreferredPhase::Water:
+                base_entry_line_[3] = "1";
+                break;
+            case Settings::Model::PreferredPhase::Gas:
+                base_entry_line_[1] = "1";
+                break;
+            case Settings::Model::PreferredPhase::Oil:
+                base_entry_line_[2] = "1";
+                break;
+            default:
+                base_entry_line_[3] = "1";
+                break;
         }
     }
     else { // If its not a black oil model, it must be a dead oil model
@@ -64,19 +85,23 @@ QString Wellstre::createWellEntry(Model::Wells::Well *well)
         base_entry_line_[2] = "0";
 
         switch (well->preferred_phase()) {
-        case Settings::Model::PreferredPhase::Water:
-            base_entry_line_[1] = "1";
-            break;
-        case Settings::Model::PreferredPhase::Oil:
-            base_entry_line_[2] = "1";
-            break;
-        default:
-            base_entry_line_[1] = "1";
-            break;
+            case Settings::Model::PreferredPhase::Water:
+                base_entry_line_[1] = "1";
+                break;
+            case Settings::Model::PreferredPhase::Oil:
+                base_entry_line_[2] = "1";
+                break;
+            default:
+                base_entry_line_[1] = "1";
+                break;
         }
     }
 
     return base_entry_line_.join("\t");
 }
 
-}}}}}
+}
+}
+}
+}
+}
--- FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/adgprs_driver_parts/wellstre.h
+++ FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/adgprs_driver_parts/wellstre.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+   Copyright (C) 2015-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef WELLSTRE_H
 #define WELLSTRE_H
 
@@ -5,7 +24,6 @@
 #include "Model/wells/well.h"
 #include "Settings/simulator.h"
 #include <QStringList>
-
 
 namespace Simulation {
 namespace SimulatorInterfaces {
@@ -15,20 +33,25 @@ namespace AdgprsDriverParts {
 
 class Wellstre : public ECLDriverParts::ECLDriverPart
 {
-public:
-    Wellstre(QList<Model::Wells::Well *> *wells, Settings::Simulator::SimulatorFluidModel fluid_model);
+ public:
+  Wellstre(QList<Model::Wells::Well *> *wells,
+           Settings::Simulator::SimulatorFluidModel fluid_model);
 
-    // DriverPart interface
-public:
-    QString GetPartString();
+  // DriverPart interface
+ public:
+  QString GetPartString();
 
-private:
-    QString createKeyword();
-    QString createWellEntry(Model::Wells::Well *well);
-    QList<Model::Wells::Well *> *wells_;
-    Settings::Simulator::SimulatorFluidModel fluid_model_;
+ private:
+  QString createKeyword();
+  QString createWellEntry(Model::Wells::Well *well);
+  QList<Model::Wells::Well *> *wells_;
+  Settings::Simulator::SimulatorFluidModel fluid_model_;
 };
 
-}}}}}
+}
+}
+}
+}
+}
 
 #endif // WELLSTRE_H
