29b95ca1a7dcdc156fcbd125ad1322f765982e4e
Einar Baumann <einar.baumann@gmail.com>
2017-05-24 11:47:08 +0200

    Add test case for two parallel spline wells

--- FieldOpt/Model/tests/test_resource_variable_property_container.h
+++ FieldOpt/Model/tests/test_resource_variable_property_container.h
@@ -25,57 +25,94 @@
 using namespace Model::Properties;
 
 namespace TestResources {
-    class TestResourceVariablePropertyContainer {
+class TestResourceVariablePropertyContainer {
 
-    public:
-        TestResourceVariablePropertyContainer() {
-            prod_heel_x_ = new ContinousProperty(5.0);
-            prod_heel_y_ = new ContinousProperty(5.0);
-            prod_heel_z_ = new ContinousProperty(200.0);
-            prod_toe_x_ = new ContinousProperty(12);
-            prod_toe_y_ = new ContinousProperty(120.1);
-            prod_toe_z_ = new ContinousProperty(300.0);
-            prod_heel_x_->setName("SplinePoint#TESTW#heel#x");
-            prod_heel_y_->setName("SplinePoint#TESTW#heel#y");
-            prod_heel_z_->setName("SplinePoint#TESTW#heel#z");
-            prod_toe_x_->setName("SplinePoint#TESTW#toe#x");
-            prod_toe_y_->setName("SplinePoint#TESTW#toe#y");
-            prod_toe_z_->setName("SplinePoint#TESTW#toe#z");
-            varcont_prod_spline_ = new VariablePropertyContainer();
-            varcont_prod_spline_->AddVariable(prod_heel_x_);
-            varcont_prod_spline_->AddVariable(prod_heel_y_);
-            varcont_prod_spline_->AddVariable(prod_heel_z_);
-            varcont_prod_spline_->AddVariable(prod_toe_x_);
-            varcont_prod_spline_->AddVariable(prod_toe_y_);
-            varcont_prod_spline_->AddVariable(prod_toe_z_);
+ public:
+  TestResourceVariablePropertyContainer() {
+      prod_heel_x_ = new ContinousProperty(5.0);
+      prod_heel_y_ = new ContinousProperty(5.0);
+      prod_heel_z_ = new ContinousProperty(200.0);
+      prod_toe_x_ = new ContinousProperty(12);
+      prod_toe_y_ = new ContinousProperty(120.1);
+      prod_toe_z_ = new ContinousProperty(300.0);
 
-            prod_bhp_0_ = new ContinousProperty(1.0);
-            prod_bhp_10_ = new ContinousProperty(1.0);
-            prod_bhp_0_->setName("BHP#PRODUCER#0");
-            prod_bhp_10_->setName("BHP#PRODUCER#10");
-            varcont_prod_bhp_ = new VariablePropertyContainer();
-            varcont_prod_bhp_->AddVariable(prod_bhp_0_);
-            varcont_prod_bhp_->AddVariable(prod_bhp_10_);
+      inje_heel_x_ = new ContinousProperty(25.0);
+      inje_heel_y_ = new ContinousProperty(5.0);
+      inje_heel_z_ = new ContinousProperty(200.0);
+      inje_toe_x_ = new ContinousProperty(32);
+      inje_toe_y_ = new ContinousProperty(120.1);
+      inje_toe_z_ = new ContinousProperty(300.0);
+
+      prod_heel_x_->setName("SplinePoint#TESTW#heel#x");
+      prod_heel_y_->setName("SplinePoint#TESTW#heel#y");
+      prod_heel_z_->setName("SplinePoint#TESTW#heel#z");
+      prod_toe_x_->setName("SplinePoint#TESTW#toe#x");
+      prod_toe_y_->setName("SplinePoint#TESTW#toe#y");
+      prod_toe_z_->setName("SplinePoint#TESTW#toe#z");
+
+      inje_heel_x_->setName("SplinePoint#INJE#heel#x");
+      inje_heel_y_->setName("SplinePoint#INJE#heel#y");
+      inje_heel_z_->setName("SplinePoint#INJE#heel#z");
+      inje_toe_x_->setName("SplinePoint#INJE#toe#x");
+      inje_toe_y_->setName("SplinePoint#INJE#toe#y");
+      inje_toe_z_->setName("SplinePoint#INJE#toe#z");
+
+      varcont_prod_spline_ = new VariablePropertyContainer();
+      varcont_prod_spline_->AddVariable(prod_heel_x_);
+      varcont_prod_spline_->AddVariable(prod_heel_y_);
+      varcont_prod_spline_->AddVariable(prod_heel_z_);
+      varcont_prod_spline_->AddVariable(prod_toe_x_);
+      varcont_prod_spline_->AddVariable(prod_toe_y_);
+      varcont_prod_spline_->AddVariable(prod_toe_z_);
+
+      varcont_two_spline_wells_ = new VariablePropertyContainer();
+      varcont_two_spline_wells_->AddVariable(prod_heel_x_);
+      varcont_two_spline_wells_->AddVariable(prod_heel_y_);
+      varcont_two_spline_wells_->AddVariable(prod_heel_z_);
+      varcont_two_spline_wells_->AddVariable(prod_toe_x_);
+      varcont_two_spline_wells_->AddVariable(prod_toe_y_);
+      varcont_two_spline_wells_->AddVariable(prod_toe_z_);
+      varcont_two_spline_wells_->AddVariable(inje_heel_x_);
+      varcont_two_spline_wells_->AddVariable(inje_heel_y_);
+      varcont_two_spline_wells_->AddVariable(inje_heel_z_);
+      varcont_two_spline_wells_->AddVariable(inje_toe_x_);
+      varcont_two_spline_wells_->AddVariable(inje_toe_y_);
+      varcont_two_spline_wells_->AddVariable(inje_toe_z_);
+
+      prod_bhp_0_ = new ContinousProperty(1.0);
+      prod_bhp_10_ = new ContinousProperty(1.0);
+      prod_bhp_0_->setName("BHP#PRODUCER#0");
+      prod_bhp_10_->setName("BHP#PRODUCER#10");
+      varcont_prod_bhp_ = new VariablePropertyContainer();
+      varcont_prod_bhp_->AddVariable(prod_bhp_0_);
+      varcont_prod_bhp_->AddVariable(prod_bhp_10_);
 
 
 
-        }
+  }
 
-        VariablePropertyContainer *varcont_prod_spline_;
-        VariablePropertyContainer *varcont_prod_bhp_;
-        VariablePropertyContainer *varcont_6r_;
+  VariablePropertyContainer *varcont_prod_spline_;
+  VariablePropertyContainer *varcont_two_spline_wells_;
+  VariablePropertyContainer *varcont_prod_bhp_;
+  VariablePropertyContainer *varcont_6r_;
 
-    private:
-        ContinousProperty *prod_heel_x_;
-        ContinousProperty *prod_heel_y_;
-        ContinousProperty *prod_heel_z_;
-        ContinousProperty *prod_toe_x_;
-        ContinousProperty *prod_toe_y_;
-        ContinousProperty *prod_toe_z_;
+ private:
+  ContinousProperty *prod_heel_x_;
+  ContinousProperty *prod_heel_y_;
+  ContinousProperty *prod_heel_z_;
+  ContinousProperty *prod_toe_x_;
+  ContinousProperty *prod_toe_y_;
+  ContinousProperty *prod_toe_z_;
+  ContinousProperty *inje_heel_x_;
+  ContinousProperty *inje_heel_y_;
+  ContinousProperty *inje_heel_z_;
+  ContinousProperty *inje_toe_x_;
+  ContinousProperty *inje_toe_y_;
+  ContinousProperty *inje_toe_z_;
 
-        ContinousProperty *prod_bhp_0_;
-        ContinousProperty *prod_bhp_10_;
-    };
+  ContinousProperty *prod_bhp_0_;
+  ContinousProperty *prod_bhp_10_;
+};
 }
 
 #endif //FIELDOPT_TEST_RESOURCE_VARIABLE_PROPERTY_LIST_H
