a445dad5967ffb74e125b0246e55347a19181747
Einar Baumann <einar.baumann@gmail.com>
2017-08-22 10:51:26 +0200

    Implemented bound-constraint things

--- FieldOpt/Optimization/constraints/bhp_constraint.h
+++ FieldOpt/Optimization/constraints/bhp_constraint.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+   Copyright (C) 2016-2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+   Modified 2017/08/22 by Einar Einar J.M. Baumann
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
 #ifndef BOXCONSTRAINT_H
 #define BOXCONSTRAINT_H
 
@@ -22,6 +42,9 @@ class BhpConstraint : public Constraint
  public:
   bool CaseSatisfiesConstraint(Case *c);
   void SnapCaseToConstraints(Case *c);
+  bool IsBoundConstraint() const override;
+  Eigen::VectorXd GetLowerBounds(QList<QUuid> id_vector) const override;
+  Eigen::VectorXd GetUpperBounds(QList<QUuid> id_vector) const override;
 
  private:
   double min_;
