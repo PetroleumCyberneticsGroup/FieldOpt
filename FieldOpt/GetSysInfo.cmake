8704308aa051d491fb64fe04caa115225b97c951
bellout <mathias.bellout@ntnu.no>
2018-03-11 20:32:56 +0100

    Cleaning up CMakeLists.txt files + making lib_wic_rixx.so work.

--- FieldOpt/GetSysInfo.cmake
+++ FieldOpt/GetSysInfo.cmake
@@ -1,6 +1,11 @@
+# Dbg ==================================================================
+message("\n============================================================")
+message(STATUS "PROJECT_NAME => ${PROJECT_NAME}")
+message(STATUS "PROJECT_SOURCE_DIR: ${PROJECT_SOURCE_DIR}")
+message(STATUS "CMAKE_BINARY_DIR: ${CMAKE_BINARY_DIR}")
 
 # System info ==========================================================
-message("=============================================================")
+message(".............................................................")
 cmake_host_system_information(RESULT HOST_HOSTNAME QUERY HOSTNAME)
 cmake_host_system_information(RESULT HOST_NUMBER_OF_LOGICAL_CORES QUERY NUMBER_OF_LOGICAL_CORES)
 cmake_host_system_information(RESULT HOST_NUMBER_OF_PHYSICAL_CORES QUERY NUMBER_OF_PHYSICAL_CORES)
