96aa2cb82e76bfb1977f5dea8030912424c76cf6
joakimra <jra93@hotmail.com>
2018-03-12 17:52:03 +0100

    Adding a new optimizer class

--- FieldOpt/Optimization/optimizers/DFO.cpp
+++ FieldOpt/Optimization/optimizers/DFO.cpp
@@ -11,13 +11,38 @@ DFO::DFO(Settings::Optimizer *settings,
          Optimization::Case *base_case,
          Model::Properties::VariablePropertyContainer *variables,
          Reservoir::Grid::Grid *grid,
-         Logger *logger) : Optimizer(settings, base_case, variables, grid, logger) {
+         Logger *logger)
+    : Optimizer(settings, base_case, variables, grid, logger) {
     // Set paramaters and stuff here
-    if (settings->parameters().trust_radius > 0.0)
-        trust_radius_ = settings->parameters().trust_radius;
-    else
-        trust_radius_ = 1.0;
+  if (settings->parameters().initial_trust_region_radius > 0.0)
+    trust_radius_ = settings->parameters().initial_trust_region_radius;
+  else
+    trust_radius_ = 1.0;
+
+  cout << "[DFO constructor] ..." << endl;
+  settings_ = settings;
+
+  cout << settings_->verb_vector()[0] << endl;
+  realvar_uuid_ = GetTentativeBestCase()->GetRealVarIdVector();
+
+
+  Eigen::VectorXd vars = base_case->GetRealVarVector();
+
+  cout<< "realvar \n" << vars << endl;
+
+
+  vars[0] += 100;
+  vars[1] += 100;
+  vars[2] += 100;
+  vars[3] += 100;
+  vars[4] += 100;
+  vars[5] += 100;
+  Optimization::Case *newCase = new Case(base_case);
+  newCase->SetRealVarValues(vars);
+  newCase->set_objective_function_value(std::numeric_limits<double>::max());
+  case_handler_->AddNewCase(newCase);
 }
+/*
 Optimization::Optimizer::TerminationCondition DFO::IsFinished() {
     return TerminationCondition::NOT_FINISHED;
 }
@@ -27,6 +52,6 @@ void DFO::handleEvaluatedCase(Optimization::Case *c) {
 void DFO::iterate() {
 
 }
-
+*/
 }
 }
--- FieldOpt/Optimization/optimizers/DFO.h
+++ FieldOpt/Optimization/optimizers/DFO.h
@@ -21,15 +21,34 @@ class DFO : public Optimizer {
       Reservoir::Grid::Grid *grid,
       Logger *logger);
 
-  TerminationCondition IsFinished() override;
+  //TerminationCondition IsFinished() override;
+  QString GetStatusStringHeader() const {};
+  QString GetStatusString() const {};
  private:
-  void iterate() override;
+  //void iterate() override;
+  void iterate() {
+    cout << "just did an ITERATE" << endl;
+  };
+  bool is_successful_iteration(){};
+
+  void handleEvaluatedCase(Case *c) override {
+    cout << "just handled an evaluated case!" << endl;
+  };
+  TerminationCondition IsFinished() {
+    return TerminationCondition::NOT_FINISHED;
+  };
+
 
  protected:
-  void handleEvaluatedCase(Case *c) override;
+  //void handleEvaluatedCase(Case *c) override;
 
  private:
   double trust_radius_;
+  QList<QUuid> realvar_uuid_;
+  Settings::Optimizer *settings_;
+
+
+
 };
 
 }
