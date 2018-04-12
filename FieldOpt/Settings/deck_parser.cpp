e89e6de9f603182efcbac3b2b537e9382b27e969
Einar Baumann <einar.baumann@gmail.com>
2018-04-12 15:25:06 +0200

    Parser implemented
    This class uses the parser from the OPM common library
    to convert the schedule section from ECL-like decks to
    the structs used in the Model settings.
    
    I still need to write tests for this.

--- FieldOpt/Settings/deck_parser.cpp
+++ FieldOpt/Settings/deck_parser.cpp
@@ -18,7 +18,9 @@
    along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 #include "deck_parser.h"
-#include <ctime>
+#include <numeric>
+#include <boost/lexical_cast.hpp>
+#include "Utilities/time.hpp"
 
 namespace Settings {
 
@@ -44,14 +46,239 @@ DeckParser::DeckParser(std::string deck_file) {
     time_map_ = &opm_schedule.getTimeMap();
     num_timesteps_ = time_map_->numTimesteps();
 
-    // Print time steps as dates (YYYY.MM.DD)
-//    for (int t = 0; t < num_timesteps_; ++t) {
-//        time_t time = time_map_->getStartTime(t);
-//        std::tm * ptm = std::localtime(&time);
-//        char buffer[12];
-//        std::strftime(buffer, 12, "%Y.%m.%d", ptm);
-//        std::cout << buffer << std::endl;
-//    }
+    initializeTimeVectors();
+
+}
+
+void DeckParser::initializeTimeVectors() {
+    time_days_ = std::vector<int>(num_timesteps_);
+    time_dates_ = std::vector<std::string>(num_timesteps_);
+    time_days_[0] = 0;
+    time_dates_[0] = unix_time_to_datestring(time_map_->getStartTime(0));
+    for (int i = 1; i < num_timesteps_; ++i) {
+        int seconds_from_start = time_map_->getStartTime(i) - time_map_->getStartTime(0);
+        int days_from_start = seconds_from_start / 24 / 60 / 60;
+        time_days_[i] = days_from_start;
+        time_dates_[i] = unix_time_to_datestring(time_map_->getStartTime(i));
+    }
+}
+
+std::vector<Model::Well> DeckParser::GetWellData() {
+    std::vector<Model::Well> well_structs = std::vector<Model::Well>(num_wells_);
+    for (int i = 0; i < num_wells_; ++i) {
+        well_structs[i] = opmWellToWellStruct(wells_[i]);
+    }
+    return well_structs;
+}
+
+Model::Well DeckParser::opmWellToWellStruct(const Opm::Well *opm_well) {
+    Model::Well well_struct;
+    well_struct.name = QString::fromStdString(opm_well->name());
+    well_struct.type = determineWellType(opm_well);
+    well_struct.preferred_phase = determinePreferredPhase(opm_well);
+    well_struct.wellbore_radius = determineWellboreRadius(opm_well);
+    well_struct.well_blocks = opmToWellBlocks(opm_well->getCompletions());
+    well_struct.controls = opmToControlEntries(opm_well);
+
+    return well_struct;
+}
+
+Model::WellType DeckParser::determineWellType(const Opm::Well *opm_well) {
+    bool is_injector = false;
+    bool is_producer = false;
+
+    for (int t = 0; t < num_timesteps_; ++t) {
+        if (!is_injector && opm_well->isInjector(t)) {
+            if (is_producer) {
+                std::cerr << "WARNING: Well " << opm_well->name()
+                          << " is detected as an alternating produer/injector well."
+                              " This is not currently supported."
+                              " Ignoring switch from producer to injector. "
+                          << time_dates_[t]
+                          << std::endl;
+                continue;
+            }
+            else {
+                is_injector = true;
+                std::cout << "Well " << opm_well->name() << " set as injector "
+                          << time_dates_[t] << std::endl;
+            }
+        }
+        if (!is_producer && opm_well->isProducer(t)) {
+            if (is_injector) {
+                std::cerr << "WARNING: Well " << opm_well->name()
+                          << " is detected as an alternating produer/injector well."
+                              " This is not currently supported."
+                              " Ignoring switch from injector to producer."
+                          << time_dates_[t]
+                          << std::endl;
+                continue;
+            }
+            is_producer = true;
+            std::cout << "Well " << opm_well->name() << " set as producer "
+                      << time_dates_[t] << std::endl;
+        }
+    }
+
+    if (is_injector && !is_producer) {
+        return Model::WellType::Injector;
+    }
+    else if (!is_injector && is_producer) {
+        return Model::WellType::Producer;
+    }
+    else {
+        std::cerr << "WARNING: Deck parser was unable to determine well type." << std::endl;
+        return Model::WellType::UNKNOWN_TYPE;
+    }
+}
+
+Model::PreferredPhase DeckParser::determinePreferredPhase(const Opm::Well *opm_well) {
+    Opm::Phase opm_phase = opm_well->getPreferredPhase();
+    switch (opm_phase)
+    {
+        case Opm::Phase::OIL:
+            return Model::PreferredPhase::Oil;
+            break;
+        case Opm::Phase::WATER:
+            return Model::PreferredPhase::Water;
+            break;
+        case Opm::Phase::GAS:
+            return Model::PreferredPhase::Gas;
+            break;
+        default:
+            std::cerr << "WARNING: Deck parser was unable to determine the preferred phase"
+                "for well " << opm_well->name() << std::endl;
+            return Model::PreferredPhase::UNKNOWN_PHASE;
+    }
+}
+
+double DeckParser::determineWellboreRadius(const Opm::Well *opm_well) {
+    auto opm_comps = opm_well->getCompletions();
+    double radii_sum = 0;
+    for (auto comp : opm_comps) {
+        radii_sum += comp.getDiameter() / 2.0;
+    }
+    double avg_radius = radii_sum / (double)opm_comps.size();
+    std::cout << "Wellbore radius set to " << boost::lexical_cast<std::string>(avg_radius)
+              << " for well " << opm_well->name() << std::endl;
+    return avg_radius;
+}
+
+QList<Model::Well::WellBlock> DeckParser::opmToWellBlocks(const Opm::CompletionSet &comp_set) {
+    auto well_blocks = QList<Model::Well::WellBlock>();
+    for (auto opm_comp : comp_set) {
+        Model::Well::Completion comp;
+        comp.type = Model::WellCompletionType::Perforation;
+        if (opm_comp.getConnectionTransmissibilityFactorAsValueObject().hasValue()) {
+            comp.transmissibility_factor = opm_comp.getConnectionTransmissibilityFactor();
+        }
+        else {
+            comp.transmissibility_factor = -1;
+        }
+        Model::Well::WellBlock wb;
+        wb.completion = comp;
+        wb.has_completion = true;
+        wb.i = opm_comp.getI();
+        wb.j = opm_comp.getJ();
+        wb.k = opm_comp.getK();
+        well_blocks.push_back(wb);
+    }
+    return well_blocks;
+}
+
+QList<Model::Well::ControlEntry> DeckParser::opmToControlEntries(const Opm::Well *opm_well) {
+    auto control_entries = QList<Model::Well::ControlEntry>();
+    for (int t = 0; t < num_timesteps_; ++t) {
+        Model::Well::ControlEntry ce;
+        ce.state = Model::WellState::WellOpen;
+        ce.control_mode = determineWellControlMode(opm_well, t);
+        ce.rate = determineRate(opm_well, t);
+        ce.bhp = determineBhp(opm_well, t);
+        if (opm_well->isInjector(t)) {
+            ce.injection_type = determineInjectorType(opm_well, t);
+        }
+        ce.time_step = time_days_[t];
+    }
+    return control_entries;
+}
+
+Model::ControlMode DeckParser::determineWellControlMode( const Opm::Well *opm_well, const int timestep ) {
+    if (opm_well->isProducer(timestep)) {
+        auto opm_wpp = opm_well->getProductionProperties(timestep);
+        switch (opm_wpp.controlMode) {
+            case Opm::WellProducer::ControlModeEnum::ORAT:
+                return Model::ControlMode::RateControl;
+                break;
+            case Opm::WellProducer::ControlModeEnum::RESV:
+                return Model::ControlMode::RateControl;
+                break;
+            case Opm::WellProducer::ControlModeEnum::LRAT:
+                return Model::ControlMode::RateControl;
+                break;
+            case Opm::WellProducer::ControlModeEnum::BHP:
+                return Model::ControlMode::BHPControl;
+                break;
+            default:
+                return Model::ControlMode::UNKNOWN_CONTROL;
+                std::cerr << "WARNING: Unable to determine control mode." << std::endl;
+        }
+    }
+    else {
+        auto opm_wip = opm_well->getInjectionProperties(timestep);
+        switch (opm_wip.controlMode) {
+            case Opm::WellInjector::ControlModeEnum::RATE:
+                return Model::ControlMode::RateControl;
+                break;
+            case Opm::WellInjector::ControlModeEnum::RESV:
+                return Model::ControlMode::RateControl;
+                break;
+            case Opm::WellInjector::ControlModeEnum::BHP:
+                return Model::ControlMode::BHPControl;
+                break;
+            default:
+                return Model::ControlMode::UNKNOWN_CONTROL;
+                std::cerr << "WARNING: Unable to determine control mode."
+                    " Unknown control type" << std::endl;
+        }
+    }
+}
+
+double DeckParser::determineRate(const Opm::Well *opm_well, const int timestep) {
+    if (opm_well->isInjector(timestep)) {
+        auto ips = opm_well->getInjectionProperties(timestep);
+        std::vector<double> rates {ips.surfaceInjectionRate, ips.reservoirInjectionRate};
+        return *std::max_element(rates.begin(), rates.end());
+    }
+    else {
+        auto pps = opm_well->getProductionProperties(timestep);
+        std::vector<double> rates {pps.OilRate, pps.LiquidRate, pps.ResVRate};
+        return *std::max_element(rates.begin(), rates.end());
+    }
+}
+
+double DeckParser::determineBhp(const Opm::Well *opm_well, const int timestep) {
+    if (opm_well->isInjector(timestep)) {
+        auto ips = opm_well->getInjectionProperties(timestep);
+        std::vector<double> bhps {ips.BHPLimit, ips.THPLimit};
+        return *std::max_element(bhps.begin(), bhps.end());
+    }
+    else {
+        auto pps = opm_well->getProductionProperties(timestep);
+        std::vector<double> bhps {pps.BHPLimit, pps.THPLimit};
+        return *std::max_element(bhps.begin(), bhps.end());
+    }
+}
+
+Model::InjectionType DeckParser::determineInjectorType(const Opm::Well *opm_well, const int timestep) {
+    auto ips = opm_well->getInjectionProperties(timestep);
+    if (ips.injectorType == Opm::WellInjector::TypeEnum::WATER)
+        return Model::InjectionType::WaterInjection;
+    else if (ips.injectorType == Opm::WellInjector::TypeEnum::GAS)
+        return Model::InjectionType::GasInjection;
+    else {
+        std::cerr << "WARNING: Unable to detect injection type. Defaulting to water." << std::endl;
+        return Model::InjectionType::WaterInjection;
+    }
 }
 
 }
--- FieldOpt/Settings/deck_parser.h
+++ FieldOpt/Settings/deck_parser.h
@@ -21,6 +21,9 @@
 #define FIELDOPT_DECKPARSER_H
 
 #include <string>
+#include <vector>
+
+#include <QList>
 
 #include <opm/parser/eclipse/Parser/Parser.hpp>
 #include <opm/parser/eclipse/Parser/MessageContainer.hpp>
@@ -30,6 +33,7 @@
 #include <opm/parser/eclipse/EclipseState/EclipseState.hpp>
 #include <opm/parser/eclipse/EclipseState/SummaryConfig/SummaryConfig.hpp>
 #include <opm/parser/eclipse/EclipseState/Schedule/Schedule.hpp>
+#include "Settings/model.h"
 
 namespace Settings {
 
@@ -46,10 +50,14 @@ namespace Settings {
  * @note This should work with ECL and Flow decks. With some tweaks,
  * it should also, in time, work with AD-GPRS decks, as their schedule
  * sections are identical.
+ *
+ * @todo Add support for multisegment wells.
  */
 class DeckParser {
  public:
   DeckParser(std::string deck_file);
+
+  std::vector<Model::Well> GetWellData();
 
  private:
   size_t num_wells_;
@@ -60,10 +68,77 @@ class DeckParser {
   const Opm::Tuning *tuning_;
   const Opm::Events *events_;
   const Opm::TimeMap *time_map_;
+  std::vector<int> time_days_;
+  std::vector<std::string> time_dates_;
 
- private:
+  void initializeTimeVectors();
 
+  /*!
+   * @brief Convert a parsed OPM well strucure to a FieldOpt
+   * Settings well struct.
+   */
+  Model::Well opmWellToWellStruct(const Opm::Well *opm_well);
 
+  /*!
+   * @brief Determine whether a well is a producer or an injector.
+   *
+   * Note that this does not support wells that alternate between
+   * producting and injecting. It will be set as whatever the well
+   * is first set to be in the deck; if it switches later a warning
+   * will be printed, but it will otherwise be ignored.
+   */
+  Model::WellType determineWellType(const Opm::Well *opm_well);
+
+  /*!
+   * @brief Determine the preferred phase for a well (water/oil/gas/liquid)
+   */
+  Model::PreferredPhase determinePreferredPhase(const Opm::Well *opm_well);
+
+  /*!
+   * @brief Determine the wellbore radius for the well.
+   *
+   * Note that this will just take the average of the wellbore radius
+   * for all the completions found for the well. This is because
+   * FieldOpt does not currently support wells with variable
+   * wellbore radius.
+   */
+  double determineWellboreRadius(const Opm::Well *opm_well);
+
+  /*!
+   * @brief Convert an OPM completion set to a list of FieldOpt settings
+   * WellBlocks.
+   */
+  QList<Model::Well::WellBlock> opmToWellBlocks(const Opm::CompletionSet &comp_set);
+
+  /*!
+   * @brief Convert OPMs representation of well controls to a list of
+   * FieldOpt Settings ControlEntries.
+   */
+  QList<Model::Well::ControlEntry> opmToControlEntries(const Opm::Well *opm_well);
+
+  /*!
+   * @brief Determine the control mode for a well at a certain timestep
+   * using the WellProductionProperties for that well at that time from
+   * the OPM parser.
+   */
+  Model::ControlMode determineWellControlMode(const Opm::Well *opm_well, const int timestep);
+
+  /*!
+   * @brief Determine the target/limit rate for a well at a timestep.
+   * @note I'm really not sure if this is correctly implemented.
+   */
+  double determineRate(const Opm::Well *opm_well, const int timestep);
+
+  /*!
+   * @brief Determine the target/limit bhp for a well at a timestep.
+   * @note I'm really not sure if this is correctly implemented.
+   */
+  double determineBhp(const Opm::Well *opm_well, const int timestep);
+
+  /*!
+   * @brief Determine the injector type (gas/water).
+   */
+  Model::InjectionType determineInjectorType(const Opm::Well *opm_well, const int timestep);
 };
 }
 
