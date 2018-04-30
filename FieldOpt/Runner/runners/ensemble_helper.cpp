673d994b70cfa1bdc44919a66263d242aea67646
Einar Baumann <einar.baumann@gmail.com>
2018-04-30 15:37:20 +0200

    Add ensemble helper class

---
+++ FieldOpt/Runner/runners/ensemble_helper.cpp
@@ -0,0 +1,94 @@
+/******************************************************************************
+ * Created: 16.12.2015 2015 by einar
+ *
+ * This file is part of the FieldOpt project.
+ *
+ * Copyright (C) 2015-2015 Einar J.M. Baumann <einar.baumann@ntnu.no>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *****************************************************************************/
+
+#include <iostream>
+#include "ensemble_helper.h"
+#include "Utilities/math.hpp"
+
+namespace Runner {
+
+EnsembleHelper::EnsembleHelper(const Settings::Ensemble &ensemble) {
+    ensemble_ = ensemble;
+    current_case_ = 0;
+    rzn_queue_ = std::vector<std::string>();
+    rzn_busy_ = std::vector<std::string>();
+    n_select_ = 5;
+    rng_ = get_random_generator();
+
+    assert(n_select_ < ensemble.GetAliases().size());
+}
+void EnsembleHelper::SetActiveCase(Optimization::Case *c) {
+    if (!IsCaseDone()) {
+        std::cerr << "ERROR: Unable to set new active case before the next case is done." << std::endl;
+        throw std::runtime_error("Error in EnsembleHelper.");
+    }
+
+    current_case_ = c;
+    selectRealizations();
+}
+bool EnsembleHelper::IsCaseDone() const {
+    return rzn_queue_.empty() && rzn_busy_.empty();
+}
+bool EnsembleHelper::IsCaseAvailableForEval() const {
+    return rzn_queue_.empty();
+}
+Optimization::Case *EnsembleHelper::GetCaseForEval() {
+    if (!IsCaseAvailableForEval()) {
+        std::cerr << "ERROR: No more cases are available for evaluation." << std::endl;
+        throw std::runtime_error("Error in EnsembleHelper.");
+    }
+    auto case_copy = new Optimization::Case(current_case_);
+    std::string next_alias = rzn_queue_.back();
+    rzn_queue_.pop_back();
+    rzn_busy_.push_back(next_alias);
+    case_copy->SetEnsembleRealization(QString::fromStdString(next_alias));
+    return case_copy;
+}
+void EnsembleHelper::SubmitEvaluatedRealization(Optimization::Case *c) {
+    long alias_pos = distance(rzn_busy_.begin(), find(rzn_busy_.begin(), rzn_busy_.end(),
+                                                     c->GetEnsembleRealization().toStdString()));
+    if (alias_pos < 0 || alias_pos >= rzn_busy_.size()) {
+        std::cerr << "ERROR: Unable to find alias in list of busy realizations." << std::endl;
+        throw std::runtime_error("Error in EnsembleHelper.");
+    }
+
+    current_case_->SetRealizationOfv(c->GetEnsembleRealization(), c->objective_function_value());
+    rzn_busy_.erase(rzn_busy_.begin() + alias_pos);
+}
+Optimization::Case *EnsembleHelper::GetEvaluatedCase() {
+    if (!IsCaseDone()) {
+        std::cerr << "ERROR: Unable to get case before all selected realizations have been evaluated." << std::endl;
+        throw std::runtime_error("Error in EnsembleHelper.");
+    }
+    rzn_queue_ = std::vector<std::string>();
+    rzn_busy_ = std::vector<std::string>();
+    return current_case_;
+}
+void EnsembleHelper::selectRealizations() {
+    auto all_aliases = ensemble_.GetAliases();
+    auto indices = unique_random_integers(rng_, 0, all_aliases.size(), n_select_);
+
+    for (auto idx : indices) {
+        rzn_queue_.push_back(all_aliases[idx]);
+    }
+}
+}
---
+++ FieldOpt/Runner/runners/ensemble_helper.h
@@ -0,0 +1,124 @@
+/******************************************************************************
+ * Created: 16.12.2015 2015 by einar
+ *
+ * This file is part of the FieldOpt project.
+ *
+ * Copyright (C) 2015-2015 Einar J.M. Baumann <einar.baumann@ntnu.no>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *****************************************************************************/
+
+#ifndef FIELDOPT_ENSEMBLE_HELPER_H
+#define FIELDOPT_ENSEMBLE_HELPER_H
+
+#include <boost/random.hpp>
+#include "Settings/ensemble.h"
+#include "Optimization/case.h"
+
+namespace Runner {
+
+/*!
+ * The EnsembleHelper class contains facilities that helps the
+ * Runner classes deal with ensembles (multiple realizations).
+ */
+class EnsembleHelper {
+
+ public:
+  EnsembleHelper(const Settings::Ensemble &ensemble);
+
+  /*!
+   * Set a new active case and populate the realization queue
+   * for it. You will not be able to set a new active case
+   * until all the realizations selected for this one have been
+   * evaluated.
+   */
+  void SetActiveCase(Optimization::Case *c);
+
+  /*!
+   * Check whether all the realizations selected for the currently
+   * active case have been evaluated.
+   */
+  bool IsCaseDone() const;
+
+  /*!
+   * Chech whether there are any cases available for evaluation.
+   */
+  bool IsCaseAvailableForEval() const;
+
+  /*!
+   * Get a copy of the currently active case, with the realization
+   * tag properly set.
+   */
+  Optimization::Case *GetCaseForEval();
+
+  /*!
+   * Return a case for a specific realization to the handler.
+   * The objective function for it will be added to the array
+   * in the original case object.
+   */
+  void SubmitEvaluatedRealization(Optimization::Case *c);
+
+  /*!
+   * Get a case that has had all the selected realizations evaluated.
+   * This case will have a filled realization-ofv map.
+   * @return
+   */
+  Optimization::Case *GetEvaluatedCase();
+
+ private:
+
+  /*!
+   * Pick a set of realizations from the ensemble and add
+   * them to the queue.
+   */
+  void selectRealizations();
+
+  /*!
+   * Ensemble object containing paths for all realizations.
+   */
+  Settings::Ensemble ensemble_;
+
+  /*!
+   * The case that is currently being considered.
+   */
+  Optimization::Case *current_case_;
+
+  /*!
+   * Realization queue, containing the alias' of all realizatons
+   * that should still be evaluated for the current case.
+   */
+  std::vector<std::string> rzn_queue_;
+
+  /*!
+   * List of alias' for realizations currently being evaluated.
+   */
+  std::vector<std::string> rzn_busy_;
+
+  /*!
+   * The number of realizations that will be selected for evaluation.
+   */
+  int n_select_;
+
+  /*!
+   * RNG that will be used when selecting which realizations to evaluate.
+   */
+  boost::random::mt19937 rng_;
+
+
+};
+
+}
+
+#endif //FIELDOPT_ENSEMBLE_HELPER_H
