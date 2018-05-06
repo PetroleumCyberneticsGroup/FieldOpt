a7d8c3a0cdef71946322e59602332890b34bd64b
bellout <mathias.bellout@ntnu.no>
2018-05-06 12:57:55 +0200

    Added adg constraint files.

---
+++ FieldOpt/Optimization/constraints/adg_constraint.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bellout on 5/6/18.
+//
+
+#include "adg_constraint.h"
---
+++ FieldOpt/Optimization/constraints/adg_constraint.h
@@ -0,0 +1,12 @@
+//
+// Created by bellout on 5/6/18.
+//
+
+#ifndef FIELDOPT_ADG_CONSTRAINT_H
+#define FIELDOPT_ADG_CONSTRAINT_H
+
+class adg_constraint {
+
+};
+
+#endif //FIELDOPT_ADG_CONSTRAINT_H
