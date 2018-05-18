431ae51cfdc5a3872b99f31ebf9e2ca750d75489
Einar Baumann <einar.baumann@gmail.com>
2018-05-18 16:07:31 +0200

    Fix indentation

--- FieldOpt/Model/wells/wellbore/completions/perforation.cpp
+++ FieldOpt/Model/wells/wellbore/completions/perforation.cpp
@@ -1,27 +1,27 @@
 #include "perforation.h"
 
 namespace Model {
-    namespace Wells {
-        namespace Wellbore {
-            namespace Completions {
+namespace Wells {
+namespace Wellbore {
+namespace Completions {
 
-                Perforation::Perforation(Settings::Model::Well::Completion completion_settings,
-                                         Properties::VariablePropertyContainer *variable_container)
-                        : Completion(completion_settings)
-                {
-                    transmissibility_factor_ = new Properties::ContinousProperty(completion_settings.transmissibility_factor);
-                    if (completion_settings.is_variable) {
-                        transmissibility_factor_->setName(completion_settings.name);
-                        variable_container->AddVariable(transmissibility_factor_);
-                    }
-                }
-
-                Perforation::Perforation()
-                        : Completion(CompletionType::Perforation)
-                {
-                    transmissibility_factor_ = new Properties::ContinousProperty(0.0);
-                }
-            }
-        }
+Perforation::Perforation(Settings::Model::Well::Completion completion_settings,
+                         Properties::VariablePropertyContainer *variable_container)
+    : Completion(completion_settings)
+{
+    transmissibility_factor_ = new Properties::ContinousProperty(completion_settings.transmissibility_factor);
+    if (completion_settings.is_variable) {
+        transmissibility_factor_->setName(completion_settings.name);
+        variable_container->AddVariable(transmissibility_factor_);
     }
+}
+
+Perforation::Perforation()
+    : Completion(CompletionType::Perforation)
+{
+    transmissibility_factor_ = new Properties::ContinousProperty(0.0);
+}
+}
+}
+}
 }
