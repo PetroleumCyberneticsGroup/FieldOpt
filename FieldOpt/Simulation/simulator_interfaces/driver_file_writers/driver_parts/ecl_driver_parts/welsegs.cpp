d1201353a4e28a9b7d3ca5dfaa07476189d0eae4
Einar Baumann <einar.baumann@gmail.com>
2018-07-02 14:58:29 +0200

    Add welsegs stub

---
+++ FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/ecl_driver_parts/welsegs.cpp
@@ -0,0 +1,5 @@
+//
+// Created by einar on 7/2/18.
+//
+
+#include "welsegs.h"
---
+++ FieldOpt/Simulation/simulator_interfaces/driver_file_writers/driver_parts/ecl_driver_parts/welsegs.h
@@ -0,0 +1,12 @@
+//
+// Created by einar on 7/2/18.
+//
+
+#ifndef FIELDOPT_WELSEGS_H
+#define FIELDOPT_WELSEGS_H
+
+class Welsegs {
+
+};
+
+#endif //FIELDOPT_WELSEGS_H
