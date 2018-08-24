09cc1dca14875d9e21dee6b7f9bf71d41e9a3e64
Einar Baumann <einar.baumann@gmail.com>
2018-08-24 15:03:45 +0200

    Add verbosity definitions

---
+++ FieldOpt/Utilities/verbosity.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+   Created by einar on 8/24/18.
+   Copyright (C) 2017 Einar J.M. Baumann <einar.baumann@gmail.com>
+
+   This file is part of the FieldOpt project.
+
+   FieldOpt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FieldOpt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FieldOpt.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+#ifndef FIELDOPT_VERBOSITY_H
+#define FIELDOPT_VERBOSITY_H
+
+#define VERB_MOD 1
+#define VERB_OPT 1
+#define VERB_WIC 3
+#define VERB_SIM 1
+#define VERB_RUN 1
+#define VERB_RES 1
+#define VERB_SET 1
+
+#endif //FIELDOPT_VERBOSITY_H
