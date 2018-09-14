838059d31d3edf775b77c1334a3a3d185f6da58c
Brage Strand Kristoffersen <brage_sk@hotmail.com>
2018-09-14 17:09:07 +0200

    Removed accidentally added CMakeFiles directory - part2

--- FieldOpt/ThirdParty/libecl/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bragesk/git/PCG/FieldOpt/FieldOpt/ThirdParty/libecl
-# Build directory: /home/bragesk/git/PCG/FieldOpt/FieldOpt/ThirdParty/libecl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib)
-subdirs(applications)
--- FieldOpt/ThirdParty/libecl/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bragesk/git/PCG/FieldOpt/FieldOpt/ThirdParty/libecl/lib
-# Build directory: /home/bragesk/git/PCG/FieldOpt/FieldOpt/ThirdParty/libecl/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
