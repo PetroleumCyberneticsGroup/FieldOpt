90db82e3b20f2c79a4e3d56f147c46ab65ad87f8
Einar Baumann <einar.baumann@gmail.com>
2018-11-12 14:02:45 +0100

    Fix initialization of components
    Components are now properly initialized with the same mode as the parent

--- FieldOpt/Optimization/hybrid_optimizer.cpp
+++ FieldOpt/Optimization/hybrid_optimizer.cpp
@@ -37,6 +37,7 @@ HybridOptimizer::HybridOptimizer(Settings::Optimizer *settings,
 {
     variables_ = variables;
     grid_ = grid;
+    iteration_ = 0;
 
     assert(settings->HybridComponents().size() == 2);
     primary_settings_ = new Settings::Optimizer(settings->HybridComponents()[0]);
@@ -44,7 +45,6 @@ HybridOptimizer::HybridOptimizer(Settings::Optimizer *settings,
 
     initializeComponent(0);
     active_component_ = 0;
-    iteration_ = 0;
 
     component_improvement_found_ = true;
 
@@ -65,7 +65,7 @@ HybridOptimizer::HybridOptimizer(Settings::Optimizer *settings,
 
 Optimizer::TerminationCondition HybridOptimizer::IsFinished() {
     if (hybrid_termination_condition_ == HybridTerminationCondition::NO_IMPROVEMENT && component_improvement_found_ == false) {
-        Printer::ext_info("No improvement found in previous component run. Terminating.", "HybridOptimizer", "Optimization");
+        Printer::ext_info("No improvement found in previous component run. Terminating.", "Optimization", "HybridOptimizer");
         if (enable_logging_) {
             logger_->AddEntry(this);
         }
@@ -90,7 +90,11 @@ void HybridOptimizer::handleEvaluatedCase(Case *c) {
     if (active_component_ == 0) {
         if (isImprovement(c)) {
             if (VERB_OPT >= 1) {
-                Printer::ext_info("Found better case. Passing to primary.", "HybridOptimizer", "Optimization");
+                std::stringstream ss;
+                ss << "Found better case. Passing to primary." << "|";
+                ss << " ID: " << c->id().toString().toStdString() << "|";
+                ss << "OFV: " << c->objective_function_value();
+                Printer::ext_info(ss.str(), "Optimization", "HybridOptimizer");
                 updateTentativeBestCase(c);
             }
         }
@@ -99,13 +103,16 @@ void HybridOptimizer::handleEvaluatedCase(Case *c) {
     else {
         if (isImprovement(c)) {
             if (VERB_OPT >= 1) {
-                Printer::ext_info("Found better case. Passing to secondary.", "HybridOptimizer", "Optimization");
+                std::stringstream ss;
+                ss << "Found better case. Passing to secondary." << "|";
+                ss << " ID: " << c->id().toString().toStdString() << "|";
+                ss << "OFV: " << c->objective_function_value();
+                Printer::ext_info(ss.str(), "Optimization", "HybridOptimizer");
                 updateTentativeBestCase(c);
             }
         }
         secondary_->handleEvaluatedCase(c);
     }
-    // 6.016680E+10
 
 }
 void HybridOptimizer::iterate() {
@@ -114,16 +121,16 @@ void HybridOptimizer::iterate() {
     }
     if (active_component_ == 0) { // Primary is active.
         if (primary_->IsFinished() == TerminationCondition::NOT_FINISHED) { // Primary is not finished.
-            if (VERB_OPT >= 1) { Printer::ext_info("Iterating with primary.", "HybridOptimizer", "Optimization"); }
+            if (VERB_OPT >= 1) { Printer::ext_info("Iterating with primary.", "Optimization", "HybridOptimizer"); }
             primary_->iterate();
         }
         else { // Primary is finished. Switch to secondary.
-            if (VERB_OPT >= 1) { Printer::ext_info("Primary component converged. Switching to secondary.", "HybridOptimizer", "Optimization"); }
+            if (VERB_OPT >= 1) { Printer::ext_info("Primary component converged. Switching to secondary.", "Optimization", "HybridOptimizer"); }
+            iteration_++;
             primary_best_case_ = tentative_best_case_;
             initializeComponent(1);
             active_component_ = 1;
             secondary_->iterate();
-            iteration_++;
 
             if (iteration_ > 1 && isBetter(primary_best_case_, secondary_best_case_) == false) {
                 // If primary didn't find an improvement over previous secondary
@@ -133,11 +140,12 @@ void HybridOptimizer::iterate() {
     }
     else { // Secondary is active
         if (secondary_->IsFinished() == TerminationCondition::NOT_FINISHED) { // Secondary is not finished.
-            if (VERB_OPT >= 1) { Printer::ext_info("Iterating with secondary.", "HybridOptimizer", "Optimization"); }
+            if (VERB_OPT >= 1) { Printer::ext_info("Iterating with secondary.", "Optimization", "HybridOptimizer"); }
             secondary_->iterate();
         }
         else { // Secondary is finished. Switch back to primary.
-            if (VERB_OPT >= 1) { Printer::ext_info("Secondary component converged. Switching to primary.", "HybridOptimizer", "Optimization"); }
+            if (VERB_OPT >= 1) { Printer::ext_info("Secondary component converged. Switching to primary.", "Optimization", "HybridOptimizer"); }
+            iteration_++;
             secondary_best_case_ = tentative_best_case_;
             initializeComponent(0);
             active_component_ = 0;
@@ -163,33 +171,36 @@ void HybridOptimizer::initializeComponent(int component=0) {
         opt_settings = secondary_settings_;
         compstr = "secondary";
     }
+    opt_settings->SetRngSeed(opt_settings->parameters().rng_seed + iteration_ * 7);
+    opt_settings->set_mode(mode_);
 
     Optimizer *opt;
 
     switch (opt_settings->type()) {
         case Settings::Optimizer::OptimizerType::Compass:
-            std::cout << "Using Compass Search as " + compstr + " component in hybrid algorithm." << std::endl;
+            Printer::ext_info("Using Compass Search as " + compstr + " component in hybrid algorithm.", "Optimization", "HybridOptimizer");
             opt = new Optimizers::CompassSearch(
                 opt_settings, tentative_best_case_, variables_, grid_, logger_,
                 case_handler_, constraint_handler_
             );
             break;
         case Settings::Optimizer::OptimizerType::APPS:
-            std::cout << "Using APPS as " + compstr + " component in hybrid algorithm." << std::endl;
+            Printer::ext_info("Using APPS as " + compstr + " component in hybrid algorithm.", "Optimization", "HybridOptimizer");
             opt = new Optimizers::APPS(
                 opt_settings, tentative_best_case_, variables_, grid_, logger_,
                 case_handler_, constraint_handler_
             );
             break;
         case Settings::Optimizer::OptimizerType::GeneticAlgorithm:
-            std::cout << "Using Genetic Algorithm as " + compstr + " component in hybrid algorithm." << std::endl;
+            Printer::ext_info("Using Genetic Algorithm as " + compstr + " component in hybrid algorithm.|RNG Seed: "
+                               + Printer::num2str(opt_settings->parameters().rng_seed), "Optimization", "HybridOptimizer");
             opt = new Optimizers::RGARDD(
                 opt_settings, tentative_best_case_, variables_, grid_, logger_,
                 case_handler_, constraint_handler_
             );
             break;
         case Settings::Optimizer::OptimizerType::EGO:
-            std::cout << "Using EGO as " + compstr + " component in hybrid algorithm." << std::endl;
+            Printer::ext_info("Using EGO as " + compstr + " component in hybrid algorithm.", "Optimization", "HybridOptimizer");
             opt = new Optimizers::BayesianOptimization::EGO(
                 opt_settings, tentative_best_case_, variables_, grid_, logger_,
                 case_handler_, constraint_handler_
