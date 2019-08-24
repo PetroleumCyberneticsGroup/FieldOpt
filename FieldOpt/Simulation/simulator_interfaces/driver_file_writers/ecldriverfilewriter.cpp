ec2721411675bc4b72f255676803e5c0287ffa85
Einar Baumann <einar.baumann@gmail.com>
2019-08-24 11:23:19 +0200

    Enable using ACTIONX with well controls
    ACTIONX-style driver file writing may now be used
    with control optimization.
    This means that control optimization may now be
    performed without specifying anything but a wells
    name and the control variables.
    
    Do note that in order to avoid problems with controls
    being specified for the beginning of simulations,
    controls specified in FieldOpt for day 0 will actually
    be applied on day 1.

--- FieldOpt/Simulation/simulator_interfaces/driver_file_writers/ecldriverfilewriter.cpp
+++ FieldOpt/Simulation/simulator_interfaces/driver_file_writers/ecldriverfilewriter.cpp
@@ -80,8 +80,26 @@ std::string EclDriverFileWriter::buildActionStrings() {
             icv_actions += wsegv.GetPartString().toStdString() + "\n";
         }
     }
+    std::string ctrl_actions = "";
+    Schedule schedule = ECLDriverParts::Schedule(model_->wells(), settings_->model()->control_times(), insets_);
+    auto schedule_time_entries = schedule.GetScheduleTimeEntries();
+    for (auto entry : schedule_time_entries) {
+        int ctrl_time = 0;
+        entry.control_time == 0 ? ctrl_time = 1 : ctrl_time = entry.control_time;
+        for (QString entry_part : entry.well_controls.GetWellEntryList()) {
+            ctrl_actions += ActionX::ACTIONX("CTR_" + Printer::num2str(entry.control_time),
+                    ECLDriverParts::ActionX::ACTX_LHQuantity::Day,
+                    ECLDriverParts::ActionX::ACTX_Operator::GE, 
+                    ctrl_time,
+                    entry_part.toStdString()
+            );
+            ctrl_actions += "\n";
+        }
+    }
+
     actions += ActionX::ACTIONX("ICVS_T0", ECLDriverParts::ActionX::ACTX_LHQuantity::Day,
         ECLDriverParts::ActionX::ACTX_Operator::GE, 0, icv_actions);
+    actions += ctrl_actions;
     return actions;
 }
 
